2eaf90643f3afb7947a0127fcfe07afa43fdc5dc
Richard Chernanko <richard@applike.infochernanko>
2019-09-30 13:26:19 +0200

    add parse datetime with format function

--- pkg/timeutils/time.go
+++ pkg/timeutils/time.go
@@ -3,6 +3,7 @@ package timeutils
 import "time"
 
 var defaultDateTimeFormat = "2006-01-02T15:04:05-07:00"
+var DateTimeMysql = "2006-01-02 15:04:05"
 
 func WithDefaultDateTimeFormat(format string) {
 	defaultDateTimeFormat = format
@@ -14,6 +15,10 @@ func FormatDateTime(datetime time.Time) string {
 
 func ParseDateTime(datetime string) (time.Time, error) {
 	return time.Parse(defaultDateTimeFormat, datetime)
+}
+
+func ParseDateTimeWithFormat(format string, datetime string) (time.Time, error) {
+	return time.Parse(format, datetime)
 }
 
 func IsSameDay(a time.Time, b time.Time) bool {
