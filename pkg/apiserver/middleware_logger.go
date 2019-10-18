7e80abebf47d38f3594c70751994fa2e5e9ecbc8
Jan K <jankamieth@gmail.com>
2019-10-18 15:20:23 +0200

    apiserver: log query parameters as fields;

--- pkg/apiserver/middleware_logger.go
+++ pkg/apiserver/middleware_logger.go
@@ -22,25 +22,33 @@ func LoggingMiddleware(logger mon.Logger) gin.HandlerFunc {
 		log := chLogger.WithContext(ctx)
 
 		path := req.URL.Path
-		query := req.URL.RawQuery
 		pathRaw := getPathRaw(ginCtx)
+
+		query := req.URL.Query()
+		queryRaw := req.URL.RawQuery
+		queryParameters := make(map[string]string)
+
+		for k := range query {
+			queryParameters[k] = query.Get(k)
+		}
 
 		method := ginCtx.Request.Method
 		requestTimeNano := time.Since(start)
 		requestTimeSecond := float64(requestTimeNano) / float64(time.Second)
 
 		log = log.WithFields(mon.Fields{
-			"bytes":            ginCtx.Writer.Size(),
-			"client_ip":        ginCtx.ClientIP(),
-			"host":             req.Host,
-			"protocol":         req.Proto,
-			"request_method":   method,
-			"request_path":     path,
-			"request_query":    query,
-			"request_path_raw": pathRaw,
-			"request_time":     requestTimeSecond,
-			"scheme":           req.URL.Scheme,
-			"status":           ginCtx.Writer.Status(),
+			"bytes":                    ginCtx.Writer.Size(),
+			"client_ip":                ginCtx.ClientIP(),
+			"host":                     req.Host,
+			"protocol":                 req.Proto,
+			"request_method":           method,
+			"request_path":             path,
+			"request_path_raw":         pathRaw,
+			"request_query":            queryRaw,
+			"request_query_parameters": queryParameters,
+			"request_time":             requestTimeSecond,
+			"scheme":                   req.URL.Scheme,
+			"status":                   ginCtx.Writer.Status(),
 		})
 
 		if ginCtx.Errors == nil || len(ginCtx.Errors) == 0 {
