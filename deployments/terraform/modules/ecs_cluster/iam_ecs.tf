28c324b9aadfef1978e4c113b2135a2e36735049
marco <marco@applike.info>
2019-11-18 15:04:01 +0100

    add-iam-s3-policy

--- deployments/terraform/modules/ecs_cluster/iam_ecs.tf
+++ deployments/terraform/modules/ecs_cluster/iam_ecs.tf
@@ -77,3 +77,8 @@ resource "aws_iam_role_policy_attachment" "ecr" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.ecs-container.name
 }
+
+resource "aws_iam_role_policy_attachment" "s3" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  role       = aws_iam_role.ecs-container.name
+}
