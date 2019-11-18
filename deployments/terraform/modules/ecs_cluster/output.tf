8af4f32ec72d0fa5ab1b4a7c5f2adb288dfc723f
Sebastian Struß <struss@applike.info>
2019-11-18 10:53:07 +0100

    deployments: added name for iam roles output, so users of gosoline can also attach policies manually

--- deployments/terraform/modules/ecs_cluster/output.tf
+++ deployments/terraform/modules/ecs_cluster/output.tf
@@ -10,6 +10,14 @@ output "iam_task_role_arn" {
   value = aws_iam_role.ecs-container.arn
 }
 
+output "iam_task_role_name" {
+  value = aws_iam_role.ecs-container.name
+}
+
 output "iam_events_role_arn" {
   value = aws_iam_role.ecs-events.arn
+}
+
+output "iam_events_role_name" {
+  value = aws_iam_role.ecs-events.name
 }
