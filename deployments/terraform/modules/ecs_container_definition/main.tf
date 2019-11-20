c4254d35e24c046828ae91534ac7935e2a06bfa4
marco <marco@applike.info>
2019-11-20 12:31:47 +0100

    change-ecs-task-definition-terraform-plugin

--- deployments/terraform/modules/ecs_container_definition/main.tf
+++ deployments/terraform/modules/ecs_container_definition/main.tf
@@ -1,26 +1,57 @@
-# Environment variables are composed into the container definition at output generation time. See outputs.tf for more information.
 locals {
-  container_definition = {
-    name             = var.container_name
-    image            = var.container_image
-    memory           = "memory_sentinel_value"
-    cpu              = "cpu_sentinel_value"
-    essential        = var.essential
-    workingDirectory = var.working_directory
-    links            = var.links
-    portMappings     = var.port_mappings
-    healthCheck      = var.healthcheck
-    dockerLabels     = var.dockerLabels
+  # Sort environment variables so terraform will not try to recreate on each plan/apply
+  env_vars             = var.environment != null ? var.environment : []
+  env_vars_keys        = [for m in local.env_vars : lookup(m, "name")]
+  env_vars_values      = [for m in local.env_vars : lookup(m, "value")]
+  env_vars_as_map      = zipmap(local.env_vars_keys, local.env_vars_values)
+  sorted_env_vars_keys = sort(local.env_vars_keys)
 
-    logConfiguration = {
-      logDriver = var.log_driver
-      options   = var.log_options
+  sorted_environment_vars = [
+    for key in local.sorted_env_vars_keys :
+    {
+      name  = key
+      value = lookup(local.env_vars_as_map, key)
     }
+  ]
 
-    environment = "environment_sentinel_value"
-    secrets     = "secrets_sentinel_value"
+  # This strange-looking variable is needed because terraform (currently) does not support explicit `null` in ternary operator,
+  # so this does not work: final_environment_vars = length(local.sorted_environment_vars) > 0 ? local.sorted_environment_vars : null
+  null_value = var.environment == null ? var.environment : null
+
+  # https://www.terraform.io/docs/configuration/expressions.html#null
+  final_environment_vars = length(local.sorted_environment_vars) > 0 ? local.sorted_environment_vars : local.null_value
+
+  container_definition = {
+    name                   = var.container_name
+    image                  = var.container_image
+    essential              = var.essential
+    entryPoint             = var.entrypoint
+    command                = var.command
+    workingDirectory       = var.working_directory
+    readonlyRootFilesystem = var.readonly_root_filesystem
+    mountPoints            = var.mount_points
+    dnsServers             = var.dns_servers
+    ulimits                = var.ulimits
+    repositoryCredentials  = var.repository_credentials
+    links                  = var.links
+    volumesFrom            = var.volumes_from
+    user                   = var.user
+    dependsOn              = var.container_depends_on
+    privileged             = var.privileged
+    portMappings           = var.port_mappings
+    healthCheck            = var.healthcheck
+    firelensConfiguration  = var.firelens_configuration
+    logConfiguration       = var.log_configuration
+    memory                 = var.container_memory
+    memoryReservation      = var.container_memory_reservation
+    cpu                    = var.container_cpu
+    environment            = local.final_environment_vars
+    secrets                = var.secrets
+    dockerLabels           = var.docker_labels
+    startTimeout           = var.start_timeout
+    stopTimeout            = var.stop_timeout
+    systemControls         = var.system_controls
   }
 
-  environment = var.environment
-  secrets     = var.secrets
+  json_map = jsonencode(local.container_definition)
 }
