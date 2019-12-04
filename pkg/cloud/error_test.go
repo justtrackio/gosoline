d9d173690d032a359884c1353b52d5ee00f82128
Jan K <jankamieth@gmail.com>
2019-12-04 16:14:33 +0100

    cloud: handle used connection error;

---
+++ pkg/cloud/error_test.go
@@ -0,0 +1,54 @@
+package cloud_test
+
+import (
+	"github.com/applike/gosoline/pkg/cloud"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kinesis"
+	"github.com/stretchr/testify/assert"
+	"net"
+	"net/http"
+	"testing"
+)
+
+func TestIsUsedClosedConnectionError(t *testing.T) {
+	ln, err := net.Listen("tcp4", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		_ = ln.Close()
+	}()
+
+	cfg := &aws.Config{
+		Region:   aws.String(endpoints.EuCentral1RegionID),
+		Endpoint: aws.String(ln.Addr().String()),
+		HTTPClient: &http.Client{
+			Transport: &http.Transport{
+				DialTLS: func(network, addr string) (net.Conn, error) {
+					conn, err := net.Dial(ln.Addr().Network(), ln.Addr().String())
+
+					if err != nil {
+						return nil, err
+					}
+
+					defer func() {
+						_ = conn.Close()
+					}()
+
+					return conn, err
+				},
+			},
+		},
+	}
+
+	sess := session.Must(session.NewSession(cfg))
+
+	client := kinesis.New(sess)
+	_, err = client.ListStreams(&kinesis.ListStreamsInput{})
+
+	isClosedErr := cloud.IsUsedClosedConnectionError(err)
+
+	assert.True(t, isClosedErr)
+}
--- pkg/stream/output_kinesis_test.go
+++ pkg/stream/output_kinesis_test.go
@@ -2,10 +2,12 @@ package stream_test
 
 import (
 	"context"
+	"github.com/applike/gosoline/pkg/cloud"
 	cloudMocks "github.com/applike/gosoline/pkg/cloud/mocks"
 	monMocks "github.com/applike/gosoline/pkg/mon/mocks"
 	"github.com/applike/gosoline/pkg/stream"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/service/kinesis"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -15,22 +17,24 @@ import (
 func TestWriter_WriteEvents(t *testing.T) {
 	kinesisClient := new(cloudMocks.KinesisAPI)
 
+	successfulRecordOutput := &kinesis.PutRecordsOutput{Records: []*kinesis.PutRecordsResultEntry{}}
+	exec := cloud.NewFixedExecutor(successfulRecordOutput, nil)
+
 	logger := monMocks.NewLoggerMockedAll()
-	writer := stream.NewKinesisOutputWithInterfaces(logger, kinesisClient, &stream.KinesisOutputSettings{
+	writer := stream.NewKinesisOutputWithInterfaces(logger, kinesisClient, exec, &stream.KinesisOutputSettings{
 		StreamName: "streamName",
 	})
 
 	batch := []*stream.Message{{Body: "1"}, {Body: "2"}, {Body: "3"}}
 
-	kinesisClient.On("PutRecords", mock.Anything).Return(&kinesis.PutRecordsOutput{
+	kinesisClient.On("PutRecordsRequest", mock.Anything).Return(&request.Request{}, &kinesis.PutRecordsOutput{
 		Records: []*kinesis.PutRecordsResultEntry{{
 			ErrorCode: aws.String("error"),
 		}},
-	}, nil).Once()
+	}).Once()
 
-	kinesisClient.On("PutRecords", mock.Anything).Return(&kinesis.PutRecordsOutput{
-		Records: []*kinesis.PutRecordsResultEntry{{}},
-	}, nil).Once()
+	kinesisClient.On("PutRecordsRequest", mock.Anything).Return(
+		&request.Request{}, successfulRecordOutput).Once()
 
 	logger.On("WithFields", mock.Anything).Return(logger).Run(func(args mock.Arguments) {
 		assert.IsType(t, map[string]interface{}{}, args.Get(0))
