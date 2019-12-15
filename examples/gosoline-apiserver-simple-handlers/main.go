0ebcdcc70ee6b16ea151462a5d4f04a52d991270
Sebastian Struß <struss@applike.info>
2019-12-15 16:49:40 +0100

    examples: added example app for api server;

---
+++ examples/gosoline-apiserver-simple-handlers/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/applike/gosoline/pkg/apiserver"
+	"github.com/applike/gosoline/pkg/apiserver/auth"
+	"github.com/applike/gosoline/pkg/apiserver/crud"
+	"github.com/applike/gosoline/pkg/application"
+	"github.com/applike/gosoline/pkg/cfg"
+	"github.com/applike/gosoline/pkg/mon"
+)
+
+type myTestStruct struct {
+	Status string `json:"status"`
+}
+
+func main() {
+	app := application.New(application.WithConfigFile("config.dist.yml", "yml"))
+	app.Add("api", apiserver.New(func(config cfg.Config, logger mon.Logger, definitions *apiserver.Definitions) {
+		definitions.GET("/json-from-map", apiserver.CreateHandler(&JsonResponseFromMapHandler{}))
+		definitions.GET("/json-from-struct", apiserver.CreateHandler(&JsonResponseFromStructHandler{}))
+
+		definitions.POST("/json-handler", apiserver.CreateJsonHandler(&JsonInputHandler{}))
+
+		group := definitions.Group("/admin")
+		group.Use(auth.NewChainHandler(map[string]auth.Authenticator{
+			"api-key":    auth.NewConfigKeyAuthenticator(config, logger, auth.ProvideValueFromHeader("X-API-KEY")),
+			"basic-auth": auth.NewBasicAuthAuthenticator(config, logger),
+		}))
+		group.GET("/authenticated", apiserver.CreateHandler(&AdminAuthenticatedHandler{}))
+
+		crud.AddCrudHandlers(definitions, 0, "/myEntity", &MyEntityHandler{
+			repo: &MyEntityRepository{},
+		})
+	}))
+	app.Run()
+}
