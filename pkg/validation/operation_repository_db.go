f565062299d50bfcfedc2615c75ee9e77031bc4d
Anselm Jonas Scholl <aj@applike.info>
2020-01-06 15:43:47 +0100

    validation, condition: merged condition module into validation module;

---
+++ pkg/validation/operation_repository_db.go
@@ -0,0 +1,48 @@
+package validation
+
+import (
+	"context"
+	"github.com/applike/gosoline/pkg/db-repo"
+)
+
+type OperationValidatingRepository struct {
+	db_repo.Repository
+	validator Validator
+}
+
+func NewOperationValidatingRepository(validator Validator, repo db_repo.Repository) db_repo.Repository {
+	return &OperationValidatingRepository{
+		Repository: repo,
+		validator:  validator,
+	}
+}
+
+func (r OperationValidatingRepository) Create(ctx context.Context, value db_repo.ModelBased) error {
+	err := r.validator.IsValid(ctx, value, db_repo.Create)
+
+	if err != nil {
+		return err
+	}
+
+	return r.Repository.Create(ctx, value)
+}
+
+func (r OperationValidatingRepository) Update(ctx context.Context, value db_repo.ModelBased) error {
+	err := r.validator.IsValid(ctx, value, db_repo.Update)
+
+	if err != nil {
+		return err
+	}
+
+	return r.Repository.Update(ctx, value)
+}
+
+func (r OperationValidatingRepository) Delete(ctx context.Context, value db_repo.ModelBased) error {
+	err := r.validator.IsValid(ctx, value, db_repo.Delete)
+
+	if err != nil {
+		return err
+	}
+
+	return r.Repository.Delete(ctx, value)
+}
