46568e5cc8871edd8b27a9264820d156eeae7cb7
Aleksandar Jelic <aleksandar@applike.info>
2020-01-16 16:28:45 +0100

    currency: replace redis with kvstore

--- pkg/currency/updater.go
+++ pkg/currency/updater.go
@@ -5,101 +5,127 @@ import (
 	"encoding/xml"
 	"github.com/applike/gosoline/pkg/cfg"
 	"github.com/applike/gosoline/pkg/http"
+	"github.com/applike/gosoline/pkg/kvstore"
 	"github.com/applike/gosoline/pkg/mon"
-	"github.com/applike/gosoline/pkg/redis"
+	"github.com/applike/gosoline/pkg/tracing"
 	"time"
+)
+
+const (
+	ExchangeRateRefresh = 8 * time.Hour
+	ExchangeRateUrl     = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
+	ExchangeRateDateKey = "currency_exchange_last_refresh"
 )
 
 type UpdaterService struct {
 	logger mon.Logger
+	tracer tracing.Tracer
 	http   http.Client
-	redis  redis.Client
+	store  kvstore.KvStore
 }
 
 func NewUpdater(config cfg.Config, logger mon.Logger) *UpdaterService {
-	redisClient := redis.GetClient(config, logger, redis.DefaultClientName)
+	logger = logger.WithChannel("currency_updater_service")
+	tracer := tracing.NewAwsTracer(config)
+	store := kvstore.NewConfigurableKvStore(config, logger, "currency")
 	httpClient := http.NewHttpClient(config, logger)
 
-	return NewUpdaterWithInterfaces(logger, redisClient, httpClient)
+	return NewUpdaterWithInterfaces(logger, tracer, store, httpClient)
 }
 
-func NewUpdaterWithInterfaces(logger mon.Logger, redisClient redis.Client, httpClient http.Client) *UpdaterService {
+func NewUpdaterWithInterfaces(logger mon.Logger, tracer tracing.Tracer, store kvstore.KvStore, httpClient http.Client) *UpdaterService {
 	return &UpdaterService{
 		logger: logger,
-		redis:  redisClient,
+		tracer: tracer,
+		store:  store,
 		http:   httpClient,
 	}
 }
 
-func (service *UpdaterService) EnsureRecentExchangeRates(ctx context.Context) error {
-	dateString, err := service.redis.Get(ExchangeRateDateKey)
+func (s *UpdaterService) EnsureRecentExchangeRates(ctx context.Context) error {
+	ctx, span := s.tracer.StartSpanFromContext(ctx, "currency-update-service")
+	defer span.Finish()
 
-	needsRefresh := false
-
-	if err != nil {
-		service.logger.Info("CurrencyUpdaterService: Error fetching redis key, refetching exchange rates")
-		needsRefresh = true
-	}
-
-	if len(dateString) == 0 {
-		service.logger.Info("CurrencyUpdaterService: Date string was zero length, refetching exchange rates")
-		needsRefresh = true
-	}
-
-	date, err := time.Parse(ExchangeRateDateFormat, dateString)
-	comparisonDate := time.Now().Add(-time.Duration(RefreshAfterHours) * time.Hour)
-
-	if err != nil {
-		service.logger.Info("CurrencyUpdaterService: Error parsing comparison date, refetching exchange rates")
-		needsRefresh = true
-	}
-
-	if err == nil && date.Before(comparisonDate) {
-		service.logger.Info("CurrencyUpdaterService: Comparison date was more than 8 hours ago, refetching exchange rates")
-		needsRefresh = true
-	}
-
-	if !needsRefresh {
+	if !s.needsRefresh(ctx) {
 		return nil
 	}
 
-	service.logger.Info("CurrencyUpdaterService: Requesting exchange rates")
-	request := service.http.NewRequest().
-		WithUrl(ExchangeRateUrl)
-
-	response, err := service.http.Get(ctx, request)
+	s.logger.Info("requesting exchange rates")
+	rates, err := s.getCurrencyRates(ctx)
 
 	if err != nil {
-		service.logger.Error(err, "CurrencyUpdaterService: Error while requesting exchange rates")
+		s.logger.Error(err, "error getting currency exchange rates")
 		return err
+	}
+
+	for _, rate := range rates {
+		err := s.store.Put(ctx, rate.Currency, rate.Rate)
+
+		if err != nil {
+			s.logger.Error(err, "error setting exchange rate")
+			return err
+		}
+
+		s.logger.Infof("currency: %s, rate: %f", rate.Currency, rate.Rate)
+	}
+
+	newTime := time.Now()
+	err = s.store.Put(ctx, ExchangeRateDateKey, newTime)
+
+	if err != nil {
+		s.logger.Error(err, "error setting refresh date")
+	}
+
+	s.logger.Info("new exchange rates are set")
+	return nil
+}
+
+func (s *UpdaterService) needsRefresh(ctx context.Context) bool {
+	var date time.Time
+	exists, err := s.store.Get(ctx, ExchangeRateDateKey, &date)
+
+	if err != nil {
+		s.logger.Info("error fetching date")
+
+		return true
+	}
+
+	if !exists {
+		s.logger.Info("date doesn't exist")
+
+		return true
+	}
+
+	comparisonDate := time.Now().Add(-ExchangeRateRefresh)
+
+	if date.Before(comparisonDate) {
+		s.logger.Info("comparison date was more than 8 hours ago")
+
+		return true
+	}
+
+	return false
+}
+
+func (s *UpdaterService) getCurrencyRates(ctx context.Context) ([]Rate, error) {
+	request := s.http.NewRequest().WithUrl(ExchangeRateUrl)
+
+	response, err := s.http.Get(ctx, request)
+
+	if err != nil {
+		s.logger.Error(err, "error requesting exchange rates")
+
+		return nil, err
 	}
 
 	exchangeRateResult := ExchangeResponse{}
 	err = xml.Unmarshal(response.Body, &exchangeRateResult)
 
 	if err != nil {
-		service.logger.Error(err, "CurrencyUpdaterService: Error while unmarshalling exchange rates")
-		return err
+		s.logger.Error(err, "error unmarshalling exchange rates")
+
+		return nil, err
 	}
 
-	for _, Cube := range exchangeRateResult.Body.Content.Rates {
-		err := service.redis.HSet(ExchangeRateDataKey, Cube.Currency, Cube.Rate)
-
-		if err != nil {
-			service.logger.Error(err, "CurrencyUpdaterService: Error while setting exchange rate")
-			return err
-		}
-
-		service.logger.Infof("CurrencyUpdaterService: Currency: %s, Rate: %f", Cube.Currency, Cube.Rate)
-	}
-
-	newTime := time.Now().Format(ExchangeRateDateFormat)
-	err = service.redis.Set(ExchangeRateDateKey, newTime, time.Duration(24)*time.Hour)
-	if err != nil {
-		service.logger.Error(err, "CurrencyUpdaterService: Failed setting refresh date")
-	}
-
-	service.logger.Info("CurrencyUpdaterService: New exchange rates are set")
-
-	return nil
+	return exchangeRateResult.Body.Content.Rates, nil
 }
