1b7794fa93badb339b1902f5b93603c69e2b956a
Jan K <jankamieth@gmail.com>
2020-01-24 14:04:14 +0100

    apiserver: set metric priority to high;

--- pkg/apiserver/handler_metric.go
+++ pkg/apiserver/handler_metric.go
@@ -4,30 +4,43 @@ import (
 	"fmt"
 	"github.com/applike/gosoline/pkg/mon"
 	"github.com/gin-gonic/gin"
+	"sync/atomic"
 	"time"
 )
 
 const (
-	MetricApiRequestCount   = "ApiRequestCount"
-	MetricApiRequestLatency = "ApiRequestLatency"
+	MetricApiRequestCount        = "ApiRequestCount"
+	MetricApiRequestResponseTime = "ApiRequestResponseTime"
+	MetricApiRequestConcurrent   = "ApiRequestConcurrent"
 )
 
 func CreateMetricHandler(definition Definition) gin.HandlerFunc {
 	defaults := getMetricMiddlewareDefaults(definition)
 	writer := mon.NewMetricDaemonWriter(defaults...)
 
+	var concurrentRequests int32 = 0
+
 	return func(ginCtx *gin.Context) {
 		start := time.Now()
+		atomic.AddInt32(&concurrentRequests, 1)
 
 		ginCtx.Next()
 
 		requestTimeNano := time.Since(start)
 		requestTimeMillisecond := float64(requestTimeNano) / float64(time.Millisecond)
-		status := ginCtx.Writer.Status() / 100
-		statusMetric := fmt.Sprintf("ApiStatus%dXX", status)
 
 		writer.WriteOne(&mon.MetricDatum{
-			Priority:   mon.PriorityLow,
+			Priority:   mon.PriorityHigh,
+			MetricName: MetricApiRequestResponseTime,
+			Dimensions: mon.MetricDimensions{
+				"path": definition.getAbsolutePath(),
+			},
+			Unit:  mon.UnitMilliseconds,
+			Value: requestTimeMillisecond,
+		})
+
+		writer.WriteOne(&mon.MetricDatum{
+			Priority:   mon.PriorityHigh,
 			MetricName: MetricApiRequestCount,
 			Dimensions: mon.MetricDimensions{
 				"path": definition.getAbsolutePath(),
@@ -36,15 +49,18 @@ func CreateMetricHandler(definition Definition) gin.HandlerFunc {
 			Value: 1.0,
 		})
 
+		cr := atomic.LoadInt32(&concurrentRequests)
+		atomic.AddInt32(&concurrentRequests, -1)
+
 		writer.WriteOne(&mon.MetricDatum{
-			Priority:   mon.PriorityLow,
-			MetricName: MetricApiRequestLatency,
-			Dimensions: mon.MetricDimensions{
-				"path": definition.getAbsolutePath(),
-			},
-			Unit:  mon.UnitMilliseconds,
-			Value: requestTimeMillisecond,
+			Priority:   mon.PriorityHigh,
+			MetricName: MetricApiRequestConcurrent,
+			Unit:       mon.UnitCountAverage,
+			Value:      float64(cr),
 		})
+
+		status := ginCtx.Writer.Status() / 100
+		statusMetric := fmt.Sprintf("ApiStatus%dXX", status)
 
 		writer.WriteOne(&mon.MetricDatum{
 			Priority:   mon.PriorityLow,
@@ -62,13 +78,21 @@ func getMetricMiddlewareDefaults(definition Definition) mon.MetricData {
 	defaults := make(mon.MetricData, 0)
 
 	metric := &mon.MetricDatum{
-		Priority:   mon.PriorityLow,
+		Priority:   mon.PriorityHigh,
 		MetricName: MetricApiRequestCount,
 		Dimensions: mon.MetricDimensions{
 			"path": definition.getAbsolutePath(),
 		},
 		Unit:  mon.UnitCount,
 		Value: 0.0,
+	}
+	defaults = append(defaults, metric)
+
+	metric = &mon.MetricDatum{
+		Priority:   mon.PriorityHigh,
+		MetricName: MetricApiRequestConcurrent,
+		Unit:       mon.UnitCountAverage,
+		Value:      0.0,
 	}
 	defaults = append(defaults, metric)
 
