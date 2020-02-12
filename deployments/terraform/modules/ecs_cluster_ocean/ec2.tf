4960d8f40f7ac9b9a223fef7f217624392ff9714
marco <marco@applike.info>
2020-02-12 18:28:15 +0100

    tf: change-ecs-cluster-config;

--- deployments/terraform/modules/ecs_cluster_ocean/ec2.tf
+++ deployments/terraform/modules/ecs_cluster_ocean/ec2.tf
@@ -4,36 +4,45 @@ resource "spotinst_ocean_ecs" "ocean" {
   cluster_name = "${var.project}-${var.environment}-${var.family}"
   region       = "eu-central-1"
   max_size     = 1000
-  min_size     = 0
+  min_size     = 1
   subnet_ids   = data.aws_subnet_ids.private.ids
 
   user_data = <<EOF
 #!/bin/bash
 echo ECS_INSTANCE_ATTRIBUTES='{"lifecycle":"spot"}' >> /etc/ecs/ecs.config
 echo ECS_CLUSTER=${var.project}-${var.environment}-${var.family} >> /etc/ecs/ecs.config
-echo ECS_ENGINE_TASK_CLEANUP_WAIT_DURATION=5m >> /etc/ecs/ecs.config
-echo ECS_NUM_IMAGES_DELETE_PER_CYCLE=50 >> /etc/ecs/ecs.config
-echo ECS_IMAGE_CLEANUP_INTERVAL=5m >> /etc/ecs/ecs.config
+echo ECS_ENGINE_TASK_CLEANUP_WAIT_DURATION=1m >> /etc/ecs/ecs.config
+echo ECS_NUM_IMAGES_DELETE_PER_CYCLE=100 >> /etc/ecs/ecs.config
+echo ECS_IMAGE_CLEANUP_INTERVAL=10m >> /etc/ecs/ecs.config
 echo ECS_UPDATES_ENABLED=true >> /etc/ecs/ecs.config
-echo ECS_RESERVED_MEMORY=1024 >> /etc/ecs/ecs.config
+echo ECS_RESERVED_MEMORY=256 >> /etc/ecs/ecs.config
 echo ECS_AVAILABLE_LOGGING_DRIVERS=[\"json-file\", \"none\", \"gelf\", \"awslogs\", \"fluentd\"] >> /etc/ecs/ecs.config
 echo ECS_ENABLE_CONTAINER_METADATA=true >> /etc/ecs/ecs.config
-cloud-init-per once docker_options echo 'OPTIONS="$${OPTIONS} --storage-opt dm.basesize=${var.ec2_block_device_size - 2}G"' >> /etc/sysconfig/docker
+echo ECS_IMAGE_PULL_BEHAVIOR=once >> /etc/ecs/ecs.config
+echo ECS_ENABLE_UNTRACKED_IMAGE_CLEANUP=true >> /etc/ecs/ecs.config
+echo ECS_ENABLE_SPOT_INSTANCE_DRAINING=true >> /etc/ecs/ecs.config
+echo ECS_CONTAINER_START_TIMEOUT=10m >> /etc/ecs/ecs.config
 
 EOF
 
-  image_id                    = var.ec2_ami
+  image_id                    = data.aws_ssm_parameter.ami.value
   security_group_ids          = data.aws_security_groups.private.ids
   key_pair                    = var.ec2_key_name
   iam_instance_profile        = aws_iam_instance_profile.ec2.id
   associate_public_ip_address = false
-  draining_timeout            = 120
+  draining_timeout            = 600
   monitoring                  = false
   ebs_optimized               = true
 
   autoscaler {
     is_enabled     = true
-    is_auto_config = true
+    is_auto_config = false
+
+    headroom {
+      cpu_per_unit    = 1000
+      memory_per_unit = 2000
+      num_of_units    = 1
+    }
 
     down {
       max_scale_down_percentage = 25
@@ -92,22 +101,25 @@ resource "spotinst_elastigroup_aws" "main" {
 #!/bin/bash
 echo ECS_INSTANCE_ATTRIBUTES='{"lifecycle":"od"}' >> /etc/ecs/ecs.config
 echo ECS_CLUSTER=${var.project}-${var.environment}-${var.family} >> /etc/ecs/ecs.config
-echo ECS_ENGINE_TASK_CLEANUP_WAIT_DURATION=5m >> /etc/ecs/ecs.config
-echo ECS_NUM_IMAGES_DELETE_PER_CYCLE=50 >> /etc/ecs/ecs.config
-echo ECS_IMAGE_CLEANUP_INTERVAL=5m >> /etc/ecs/ecs.config
+echo ECS_ENGINE_TASK_CLEANUP_WAIT_DURATION=1m >> /etc/ecs/ecs.config
+echo ECS_NUM_IMAGES_DELETE_PER_CYCLE=100 >> /etc/ecs/ecs.config
+echo ECS_IMAGE_CLEANUP_INTERVAL=10m >> /etc/ecs/ecs.config
 echo ECS_UPDATES_ENABLED=true >> /etc/ecs/ecs.config
-echo ECS_RESERVED_MEMORY=1024 >> /etc/ecs/ecs.config
+echo ECS_RESERVED_MEMORY=256 >> /etc/ecs/ecs.config
 echo ECS_AVAILABLE_LOGGING_DRIVERS=[\"json-file\", \"none\", \"gelf\", \"awslogs\", \"fluentd\"] >> /etc/ecs/ecs.config
 echo ECS_ENABLE_CONTAINER_METADATA=true >> /etc/ecs/ecs.config
-cloud-init-per once docker_options echo 'OPTIONS="$${OPTIONS} --storage-opt dm.basesize=${var.ec2_block_device_size - 2}G"' >> /etc/sysconfig/docker
+echo ECS_IMAGE_PULL_BEHAVIOR=once >> /etc/ecs/ecs.config
+echo ECS_ENABLE_UNTRACKED_IMAGE_CLEANUP=true >> /etc/ecs/ecs.config
+echo ECS_ENABLE_SPOT_INSTANCE_DRAINING=true >> /etc/ecs/ecs.config
+echo ECS_CONTAINER_START_TIMEOUT=10m >> /etc/ecs/ecs.config
 
 EOF
 
-  image_id             = var.ec2_ami
+  image_id             = data.aws_ssm_parameter.ami.value
   security_groups      = data.aws_security_groups.private.ids
   key_name             = var.ec2_key_name
   iam_instance_profile = aws_iam_instance_profile.ec2.id
-  draining_timeout     = 120
+  draining_timeout     = 300
   enable_monitoring    = false
   ebs_optimized        = true
 
@@ -126,8 +138,10 @@ EOF
   }
 
   integration_ecs {
-    cluster_name         = "${var.project}-${var.environment}-${var.family}"
-    autoscale_is_enabled = true
+    cluster_name             = "${var.project}-${var.environment}-${var.family}"
+    autoscale_is_enabled     = true
+    autoscale_cooldown       = 600
+    autoscale_is_auto_config = true
   }
 
   tags {
@@ -156,11 +170,7 @@ EOF
   }
 
   update_policy {
-    should_roll            = true
+    should_roll            = false
     should_resume_stateful = false
-
-    roll_config {
-      batch_size_percentage = 25
-    }
   }
 }
