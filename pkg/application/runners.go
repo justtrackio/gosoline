a0eef3d4ae7b3f388ab3baa48dd97a4c623c1e5a
Anselm Jonas Scholl <aj@justdice.io>
2020-06-26 11:33:30 +0200

    application: added shorthand function to run subscribers;

--- pkg/application/runners.go
+++ pkg/application/runners.go
@@ -1,6 +1,9 @@
 package application
 
-import "github.com/applike/gosoline/pkg/stream"
+import (
+	"github.com/applike/gosoline/pkg/stream"
+	"github.com/applike/gosoline/pkg/sub"
+)
 
 func RunConsumer(callback stream.ConsumerCallback, options ...Option) {
 	consumers := stream.NewConsumerFactory(map[string]stream.ConsumerCallback{
@@ -9,5 +12,14 @@ func RunConsumer(callback stream.ConsumerCallback, options ...Option) {
 
 	app := Default(options...)
 	app.AddFactory(consumers)
+	app.Run()
+}
+
+func RunSubscriber(transformers sub.TransformerMapTypeVersionFactories) {
+	app := Default()
+
+	subs := sub.NewSubscriberFactory(transformers)
+	app.AddFactory(subs)
+
 	app.Run()
 }
