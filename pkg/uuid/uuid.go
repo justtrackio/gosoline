746ade352c048286cec32176ff8709c56ef2a763
Anselm Jonas Scholl <aj@justdice.io>
2020-07-09 11:43:17 +0200

    uuid: added ValidV4 function to validate uuids;

--- pkg/uuid/uuid.go
+++ pkg/uuid/uuid.go
@@ -2,7 +2,10 @@ package uuid
 
 import (
 	"github.com/google/uuid"
+	"regexp"
 )
+
+var uuidRegExp = regexp.MustCompile("^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$")
 
 //go:generate mockery -name Uuid
 type Uuid interface {
@@ -18,4 +21,9 @@ func New() Uuid {
 // NewV4 returns a UUID v4 string
 func (u *RealUuid) NewV4() string {
 	return uuid.New().String()
+}
+
+// Check if the given string is a valid lowercase UUID v4 string
+func ValidV4(uuid string) bool {
+	return uuidRegExp.Match([]byte(uuid))
 }
