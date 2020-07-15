df11e28f29b480e1da84bdd3134a90a96abda88a
Anselm Jonas Scholl <aj@justdice.io>
2020-07-15 17:21:05 +0200

    application, clock: added clock module and made configuring for UTC the default;

---
+++ pkg/clock/clock_test.go
@@ -0,0 +1,37 @@
+package clock_test
+
+import (
+	"github.com/applike/gosoline/pkg/clock"
+	"github.com/applike/gosoline/pkg/test/assert"
+	"testing"
+	"time"
+)
+
+func TestRealClock_After(t *testing.T) {
+	clock.WithUseUTC(true)
+	c := clock.NewRealClock()
+	ch := c.After(time.Millisecond)
+	now := <-ch
+	assert.Equal(t, now.UTC(), now)
+}
+
+func TestRealClock_NowYieldsUTC(t *testing.T) {
+	clock.WithUseUTC(true)
+	c := clock.NewRealClock()
+	now := c.Now()
+	assert.Equal(t, now.UTC(), now)
+}
+
+func TestNewFakeClock(t *testing.T) {
+	c := clock.NewFakeClock()
+	now := c.Now()
+	time.Sleep(time.Millisecond)
+	assert.Equal(t, now, c.Now())
+}
+
+func TestNewFakeClockAt(t *testing.T) {
+	now := time.Now()
+	c := clock.NewFakeClockAt(now)
+	time.Sleep(time.Millisecond)
+	assert.Equal(t, now, c.Now())
+}
