0452951dcd14fa64488cf9e412cba682e871ca60
Anselm Jonas Scholl <aj@justdice.io>
2020-08-28 16:20:24 +0200

    cloud/aws: retry aws requests if a 500 status response is returned;

--- pkg/cloud/aws/error.go
+++ pkg/cloud/aws/error.go
@@ -2,6 +2,7 @@ package aws
 
 import (
 	"errors"
+	"fmt"
 	"github.com/applike/gosoline/pkg/exec"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/request"
@@ -9,6 +10,42 @@ import (
 
 func init() {
 	exec.AddRequestCancelCheck(IsAwsErrorCodeRequestCanceled)
+}
+
+type InvalidStatusError struct {
+	Status int
+}
+
+func (e *InvalidStatusError) Error() string {
+	return fmt.Sprintf("http status code: %d", e.Status)
+}
+
+func (e *InvalidStatusError) Is(err error) bool {
+	_, ok := err.(*InvalidStatusError)
+
+	return ok
+}
+
+func (e *InvalidStatusError) As(target interface{}) bool {
+	err, ok := target.(*InvalidStatusError)
+
+	if ok && err != nil {
+		*err = *e
+	}
+
+	return ok
+}
+
+func IsInvalidStatusError(err error) bool {
+	return errors.Is(err, &InvalidStatusError{})
+}
+
+func CheckInvalidStatusError(_ interface{}, err error) exec.ErrorType {
+	if IsInvalidStatusError(err) {
+		return exec.ErrorRetryable
+	}
+
+	return exec.ErrorUnknown
 }
 
 func IsAwsError(err error, awsCode string) bool {
