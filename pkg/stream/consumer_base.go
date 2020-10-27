50a6d159eef686cd31ec7e65b149d333543c9b53
Sebastian Struß <struss@applike.info>
2020-10-27 08:05:19 +0100

    stream: reworked batch consumer;

---
+++ pkg/stream/consumer_base.go
@@ -0,0 +1,234 @@
+package stream
+
+import (
+	"context"
+	"fmt"
+	"github.com/applike/gosoline/pkg/cfg"
+	"github.com/applike/gosoline/pkg/clock"
+	"github.com/applike/gosoline/pkg/coffin"
+	"github.com/applike/gosoline/pkg/kernel"
+	"github.com/applike/gosoline/pkg/mon"
+	"github.com/applike/gosoline/pkg/tracing"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+const (
+	metricNameConsumerProcessedCount = "ConsumerProcessedCount"
+	metricNameConsumerDuration       = "ConsumerDuration"
+)
+
+type InputCallback interface {
+	run(ctx context.Context) error
+}
+
+//go:generate mockery -name=RunnableCallback
+type RunnableCallback interface {
+	Run(ctx context.Context) error
+}
+
+type BaseConsumerCallback interface {
+	Boot(config cfg.Config, logger mon.Logger) error
+	GetModel(attributes map[string]interface{}) interface{}
+}
+
+type ConsumerSettings struct {
+	Input       string        `cfg:"input" default:"consumer" validate:"required"`
+	RunnerCount int           `cfg:"runner_count" default:"10" validate:"min=1"`
+	Encoding    string        `cfg:"encoding" default:"application/json"`
+	IdleTimeout time.Duration `cfg:"idle_timeout" default:"10s"`
+}
+
+type baseConsumer struct {
+	kernel.EssentialModule
+	kernel.ApplicationStage
+	ConsumerAcknowledge
+
+	logger  mon.Logger
+	encoder MessageEncoder
+	mw      mon.MetricWriter
+	tracer  tracing.Tracer
+	clock   clock.Clock
+
+	wg     sync.WaitGroup
+	cancel context.CancelFunc
+
+	id               string
+	name             string
+	settings         *ConsumerSettings
+	consumerCallback interface{}
+	inputCallback    InputCallback
+	processed        int32
+}
+
+func newBaseConsumer(name string, consumerCallback BaseConsumerCallback, inputCallback InputCallback) *baseConsumer {
+	return &baseConsumer{
+		name:             name,
+		consumerCallback: consumerCallback,
+		inputCallback:    inputCallback,
+		clock:            clock.Provider,
+	}
+}
+
+func (c *baseConsumer) Boot(config cfg.Config, logger mon.Logger) error {
+	if err := c.bootCallback(config, logger); err != nil {
+		return err
+	}
+
+	settings := &ConsumerSettings{}
+	key := ConfigurableConsumerKey(c.name)
+	config.UnmarshalKey(key, settings, cfg.UnmarshalWithDefaultForKey("encoding", defaultMessageBodyEncoding))
+
+	appId := cfg.GetAppIdFromConfig(config)
+	c.id = fmt.Sprintf("consumer-%s-%s-%s", appId.Family, appId.Application, c.name)
+
+	tracer := tracing.ProviderTracer(config, logger)
+
+	defaultMetrics := getConsumerDefaultMetrics()
+	mw := mon.NewMetricDaemonWriter(defaultMetrics...)
+
+	input := NewConfigurableInput(config, logger, settings.Input)
+	encoder := NewMessageEncoder(&MessageEncoderSettings{
+		Encoding: settings.Encoding,
+	})
+
+	c.BootWithInterfaces(logger, tracer, mw, input, encoder, settings)
+
+	return nil
+}
+
+func (c *baseConsumer) BootWithInterfaces(logger mon.Logger, tracer tracing.Tracer, mw mon.MetricWriter, input Input, encoder MessageEncoder, settings *ConsumerSettings) {
+	c.logger = logger.WithChannel("consumer")
+	c.tracer = tracer
+	c.mw = mw
+	c.input = input
+	c.ConsumerAcknowledge = NewConsumerAcknowledgeWithInterfaces(logger, c.input)
+	c.encoder = encoder
+	c.settings = settings
+}
+
+func (c *baseConsumer) bootCallback(config cfg.Config, logger mon.Logger) error {
+	loggerCallback := logger.WithChannel("consumerCallback")
+	contextEnforcingLogger := mon.NewContextEnforcingLogger(loggerCallback)
+
+	err := c.consumerCallback.(BaseConsumerCallback).Boot(config, contextEnforcingLogger)
+
+	if err != nil {
+		return fmt.Errorf("error during booting the consumer consumerCallback: %w", err)
+	}
+
+	contextEnforcingLogger.Enable()
+
+	return nil
+}
+
+func ConfigurableConsumerKey(name string) string {
+	return fmt.Sprintf("stream.consumer.%s", name)
+}
+
+func getConsumerDefaultMetrics() mon.MetricData {
+	return mon.MetricData{
+		{
+			Priority:   mon.PriorityHigh,
+			MetricName: metricNameConsumerProcessedCount,
+			Unit:       mon.UnitCount,
+			Value:      0.0,
+		},
+		{
+			Priority:   mon.PriorityHigh,
+			MetricName: metricNameConsumerDuration,
+			Unit:       mon.UnitMilliseconds,
+			Value:      0.0,
+		},
+	}
+}
+
+func (c *baseConsumer) Run(kernelCtx context.Context) error {
+	defer c.logger.Infof("leaving consumer %s", c.name)
+	c.logger.Infof("running consumer %s with input %s", c.name, c.settings.Input)
+
+	// create ctx whose done channel is closed on dying coffin
+	cfn, dyingCtx := coffin.WithContext(context.Background())
+
+	// create ctx whose done channel is closed on dying coffin and manual cancel
+	manualCtx := cfn.Context(context.Background())
+	manualCtx, c.cancel = context.WithCancel(manualCtx)
+
+	cfn.GoWithContextf(manualCtx, c.logConsumeCounter, "panic during counter log")
+	cfn.GoWithContextf(manualCtx, c.runConsumerCallback, "panic during run of the consumerCallback")
+	// run the input after the counters are running to make sure our coffin does not immediately
+	// die just because Run() immediately returns
+	cfn.GoWithContextf(dyingCtx, c.input.Run, "panic during run of the consumer input")
+
+	c.wg.Add(c.settings.RunnerCount)
+	cfn.Go(c.stopConsuming)
+
+	for i := 0; i < c.settings.RunnerCount; i++ {
+		cfn.GoWithContextf(manualCtx, c.inputCallback.run, "panic during consuming")
+	}
+
+	// stop input on kernel cancel
+	go func() {
+		<-kernelCtx.Done()
+		c.input.Stop()
+	}()
+
+	if err := cfn.Wait(); err != nil {
+		return fmt.Errorf("error while waiting for all routines to stop: %w", err)
+	}
+
+	return nil
+}
+
+func (c *baseConsumer) logConsumeCounter(ctx context.Context) error {
+	logger := c.logger.WithContext(ctx)
+	defer logger.Debug("logConsumeCounter is ending")
+
+	ticker := time.NewTicker(c.settings.IdleTimeout)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-ticker.C:
+			processed := atomic.SwapInt32(&c.processed, 0)
+
+			logger.WithFields(mon.Fields{
+				"count": processed,
+				"name":  c.name,
+			}).Infof("processed %v messages", processed)
+		}
+	}
+}
+
+func (c *baseConsumer) runConsumerCallback(ctx context.Context) error {
+	logger := c.logger.WithContext(ctx)
+	defer logger.Debug("runConsumerCallback is ending")
+
+	if runnable, ok := c.consumerCallback.(RunnableCallback); ok {
+		return runnable.Run(ctx)
+	}
+
+	return nil
+}
+
+func (c *baseConsumer) stopConsuming() error {
+	defer c.logger.Debug("stopConsuming is ending")
+
+	c.wg.Wait()
+	c.input.Stop()
+	c.cancel()
+
+	return nil
+}
+
+func (c *baseConsumer) recover() {
+	err := coffin.ResolveRecovery(recover())
+	if err == nil {
+		return
+	}
+
+	c.logger.Error(err, err.Error())
+}
---
+++ pkg/stream/consumer_batch_test.go
@@ -0,0 +1,317 @@
+package stream_test
+
+import (
+	"context"
+	"fmt"
+	"github.com/applike/gosoline/pkg/mdl"
+	monMocks "github.com/applike/gosoline/pkg/mon/mocks"
+	"github.com/applike/gosoline/pkg/stream"
+	"github.com/applike/gosoline/pkg/stream/mocks"
+	"github.com/applike/gosoline/pkg/tracing"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"sync"
+	"testing"
+	"time"
+)
+
+type acknowledgableInput struct {
+	mocks.AcknowledgeableInput
+	mocks.Input
+}
+
+type BatchConsumerTestSuite struct {
+	suite.Suite
+
+	data chan *stream.Message
+	once sync.Once
+	stop func()
+
+	input *acknowledgableInput
+
+	callback      *mocks.RunnableBatchConsumerCallback
+	batchConsumer *stream.BatchConsumer
+}
+
+func TestBatchConsumerTestSuite(t *testing.T) {
+	suite.Run(t, new(BatchConsumerTestSuite))
+}
+
+func (s *BatchConsumerTestSuite) SetupTest() {
+	s.data = make(chan *stream.Message, 10)
+	s.once = sync.Once{}
+	s.stop = func() {
+		s.once.Do(func() {
+			close(s.data)
+		})
+	}
+
+	s.input = new(acknowledgableInput)
+	s.callback = new(mocks.RunnableBatchConsumerCallback)
+
+	logger := monMocks.NewLoggerMockedAll()
+	tracer := tracing.NewNoopTracer()
+	mw := monMocks.NewMetricWriterMockedAll()
+	me := stream.NewMessageEncoder(&stream.MessageEncoderSettings{})
+	settings := &stream.ConsumerSettings{
+		Input:       "test",
+		RunnerCount: 1,
+		IdleTimeout: time.Second,
+	}
+	batchSettings := &stream.BatchConsumerSettings{
+		IdleTimeout: time.Second,
+		BatchSize:   5,
+	}
+
+	s.batchConsumer = stream.NewBatchConsumer("test", s.callback)
+	s.batchConsumer.BootWithInterfaces(logger, tracer, mw, s.input, me, settings, batchSettings)
+}
+
+func (s *BatchConsumerTestSuite) TestRun_ProcessOnStop() {
+	s.input.Input.
+		On("Data").
+		Return(s.data)
+
+	s.input.Input.
+		On("Run", mock.AnythingOfType("*context.cancelCtx")).
+		Run(func(args mock.Arguments) {
+			s.data <- stream.NewJsonMessage(`"foo"`)
+			s.data <- stream.NewJsonMessage(`"bar"`)
+			s.data <- stream.NewJsonMessage(`"foobar"`)
+			s.stop()
+		}).Return(nil)
+
+	processed := 0
+
+	s.input.Input.
+		On("Stop").
+		Return()
+
+	s.input.AcknowledgeableInput.
+		On("AckBatch", mock.AnythingOfType("[]*stream.Message")).
+		Run(func(args mock.Arguments) {
+			msgs := args[0].([]*stream.Message)
+			processed = len(msgs)
+		}).
+		Return(nil)
+
+	acks := []bool{true, true, true}
+	s.callback.On("Consume", mock.AnythingOfType("*context.cancelCtx"), mock.AnythingOfType("[]interface {}"), mock.AnythingOfType("[]map[string]interface {}")).
+		Return(acks, nil)
+
+	s.callback.On("GetModel", mock.AnythingOfType("map[string]interface {}")).
+		Return(func(_ map[string]interface{}) interface{} {
+			return mdl.String("")
+		})
+
+	s.callback.On("Run", mock.AnythingOfType("*context.cancelCtx")).
+		Return(nil)
+
+	err := s.batchConsumer.Run(context.Background())
+
+	s.NoError(err, "there should be no error during run")
+	s.Equal(3, processed)
+
+	s.input.Input.AssertExpectations(s.T())
+	s.input.AcknowledgeableInput.AssertExpectations(s.T())
+	s.callback.AssertExpectations(s.T())
+}
+
+func (s *BatchConsumerTestSuite) TestRun_BatchSizeReached() {
+	s.input.Input.
+		On("Data").
+		Return(s.data)
+
+	s.input.Input.
+		On("Run", mock.AnythingOfType("*context.cancelCtx")).
+		Run(func(args mock.Arguments) {
+			s.data <- stream.NewJsonMessage(`"foo"`)
+			s.data <- stream.NewJsonMessage(`"bar"`)
+			s.data <- stream.NewJsonMessage(`"foobar"`)
+			s.data <- stream.NewJsonMessage(`"barfoo"`)
+			s.data <- stream.NewJsonMessage(`"foobarfoo"`)
+		}).Return(nil)
+
+	s.input.Input.
+		On("Stop").
+		Return()
+
+	processed := 0
+
+	s.input.AcknowledgeableInput.
+		On("AckBatch", mock.AnythingOfType("[]*stream.Message")).
+		Run(func(args mock.Arguments) {
+			msgs := args[0].([]*stream.Message)
+			processed = len(msgs)
+
+			s.stop()
+		}).
+		Return(nil)
+
+	acks := []bool{true, true, true, true, true}
+	s.callback.On("Consume", mock.AnythingOfType("*context.cancelCtx"), mock.AnythingOfType("[]interface {}"), mock.AnythingOfType("[]map[string]interface {}")).
+		Return(acks, nil)
+
+	s.callback.On("GetModel", mock.AnythingOfType("map[string]interface {}")).
+		Return(func(_ map[string]interface{}) interface{} {
+			return mdl.String("")
+		})
+
+	s.callback.On("Run", mock.AnythingOfType("*context.cancelCtx")).
+		Return(nil)
+
+	err := s.batchConsumer.Run(context.Background())
+
+	s.NoError(err, "there should be no error during run")
+	s.Equal(5, processed)
+
+	s.input.Input.AssertExpectations(s.T())
+	s.input.AcknowledgeableInput.AssertExpectations(s.T())
+	s.callback.AssertExpectations(s.T())
+}
+
+func (s *BatchConsumerTestSuite) TestRun_ContextCanceled() {
+	ctx, cancel := context.WithCancel(context.Background())
+	stopped := make(chan struct{})
+	once := sync.Once{}
+
+	s.input.Input.
+		On("Data").
+		Return(s.data)
+
+	s.input.Input.
+		On("Run", mock.AnythingOfType("*context.cancelCtx")).
+		Run(func(args mock.Arguments) {
+			cancel()
+			<-stopped
+			s.stop()
+		}).Return(nil)
+
+	processed := 0
+
+	s.input.Input.
+		On("Stop").
+		Run(func(args mock.Arguments) {
+			once.Do(func() {
+				close(stopped)
+			})
+		}).
+		Return(nil)
+
+	s.callback.On("Run", mock.AnythingOfType("*context.cancelCtx")).
+		Return(nil)
+
+	err := s.batchConsumer.Run(ctx)
+
+	s.NoError(err, "there should be no error during run")
+	s.Equal(0, processed)
+
+	s.input.Input.AssertExpectations(s.T())
+	s.input.AcknowledgeableInput.AssertExpectations(s.T())
+	s.callback.AssertExpectations(s.T())
+}
+
+func (s *BatchConsumerTestSuite) TestRun_InputRunError() {
+	s.input.Input.
+		On("Data").
+		Return(s.data)
+
+	s.input.Input.
+		On("Run", mock.AnythingOfType("*context.cancelCtx")).
+		Return(fmt.Errorf("read error"))
+
+	s.callback.
+		On("Run", mock.AnythingOfType("*context.cancelCtx")).
+		Run(func(args mock.Arguments) {
+			<-args[0].(context.Context).Done()
+		}).Return(nil)
+
+	err := s.batchConsumer.Run(context.Background())
+
+	s.EqualError(err, "error while waiting for all routines to stop: panic during run of the consumer input: read error")
+
+	s.input.Input.AssertExpectations(s.T())
+	s.input.AcknowledgeableInput.AssertExpectations(s.T())
+	s.callback.AssertExpectations(s.T())
+}
+
+func (s *BatchConsumerTestSuite) TestRun_CallbackRunError() {
+	s.input.Input.On("Data").
+		Return(s.data)
+
+	s.input.Input.On("Run", mock.AnythingOfType("*context.cancelCtx")).
+		Run(func(args mock.Arguments) {
+			<-args[0].(context.Context).Done()
+		}).
+		Return(nil)
+
+	s.callback.On("Run", mock.AnythingOfType("*context.cancelCtx")).
+		Return(fmt.Errorf("consumerCallback run error"))
+
+	err := s.batchConsumer.Run(context.Background())
+
+	s.EqualError(err, "error while waiting for all routines to stop: panic during run of the consumerCallback: consumerCallback run error")
+
+	s.input.Input.AssertExpectations(s.T())
+	s.input.AcknowledgeableInput.AssertExpectations(s.T())
+	s.callback.AssertExpectations(s.T())
+}
+
+func (s *BatchConsumerTestSuite) TestRun_AggregateMessage() {
+	message1 := stream.NewJsonMessage(`"foo"`, map[string]interface{}{
+		"attr1": "a",
+	})
+	message2 := stream.NewJsonMessage(`"bar"`, map[string]interface{}{
+		"attr1": "b",
+	})
+
+	aggregate, err := stream.BuildAggregateMessage(stream.MarshalJsonMessage, []stream.WritableMessage{message1, message2})
+
+	s.Require().NoError(err)
+
+	s.input.Input.
+		On("Data").
+		Return(s.data)
+
+	s.input.Input.
+		On("Run", mock.AnythingOfType("*context.cancelCtx")).
+		Run(func(args mock.Arguments) {
+			s.data <- aggregate.(*stream.Message)
+			s.stop()
+		}).Return(nil)
+
+	s.input.Input.
+		On("Stop").
+		Return()
+
+	processed := 0
+	s.input.AcknowledgeableInput.
+		On("AckBatch", mock.AnythingOfType("[]*stream.Message")).
+		Run(func(args mock.Arguments) {
+			msgs := args[0].([]*stream.Message)
+			processed = len(msgs)
+		}).
+		Return()
+
+	s.callback.On("Run", mock.AnythingOfType("*context.cancelCtx")).
+		Return(nil)
+
+	acks := []bool{true, true}
+
+	s.callback.On("Consume", mock.AnythingOfType("*context.cancelCtx"), mock.AnythingOfType("[]interface {}"), mock.AnythingOfType("[]map[string]interface {}")).
+		Return(acks, nil)
+
+	s.callback.
+		On("GetModel", mock.AnythingOfType("map[string]interface {}")).
+		Return(mdl.String("")).
+		Twice()
+
+	err = s.batchConsumer.Run(context.Background())
+
+	s.Nil(err, "there should be no error returned on consume")
+	s.Equal(processed, 2)
+
+	s.input.Input.AssertExpectations(s.T())
+	s.input.AcknowledgeableInput.AssertExpectations(s.T())
+	s.callback.AssertExpectations(s.T())
+}
