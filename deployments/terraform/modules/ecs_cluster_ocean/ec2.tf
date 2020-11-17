00b2c7c23a4845e112ffb6ec998e646b880a1596
Marco Frömbgen <marco.froembgen@gmail.com>
2020-11-17 14:48:07 +0100

    tf: ecs-cluster-changes; (#363)

--- deployments/terraform/modules/ecs_cluster_ocean/ec2.tf
+++ deployments/terraform/modules/ecs_cluster_ocean/ec2.tf
@@ -10,21 +10,15 @@ resource "spotinst_ocean_ecs" "ocean" {
 #!/bin/bash
 echo ECS_INSTANCE_ATTRIBUTES='{"lifecycle":"spot"}' >> /etc/ecs/ecs.config
 echo ECS_CLUSTER=${var.project}-${var.environment}-${var.family} >> /etc/ecs/ecs.config
-echo ECS_ENGINE_TASK_CLEANUP_WAIT_DURATION=1m >> /etc/ecs/ecs.config
-echo ECS_NUM_IMAGES_DELETE_PER_CYCLE=100 >> /etc/ecs/ecs.config
-echo ECS_RESERVED_MEMORY=512 >> /etc/ecs/ecs.config
-echo ECS_AVAILABLE_LOGGING_DRIVERS=[\"json-file\", \"none\", \"gelf\", \"awslogs\", \"fluentd\"] >> /etc/ecs/ecs.config
-echo ECS_ENABLE_CONTAINER_METADATA=true >> /etc/ecs/ecs.config
-echo ECS_ENABLE_UNTRACKED_IMAGE_CLEANUP=true >> /etc/ecs/ecs.config
-echo ECS_ENABLE_SPOT_INSTANCE_DRAINING=true >> /etc/ecs/ecs.config
+echo ECS_AVAILABLE_LOGGING_DRIVERS=[\"none\", \"awslogs\", \"fluentd\"] >> /etc/ecs/ecs.config
 EOF
 
   image_id             = data.aws_ssm_parameter.ami.value
   security_group_ids   = data.aws_security_groups.private.ids
   key_pair             = var.ec2_key_name
   iam_instance_profile = aws_iam_instance_profile.ec2.id
-  draining_timeout     = 300
   ebs_optimized        = true
+  draining_timeout     = 300
 
   autoscaler {
     is_enabled     = true
@@ -33,7 +27,7 @@ EOF
     headroom {}
 
     down {
-      max_scale_down_percentage = 25
+      max_scale_down_percentage = 10
     }
 
     resource_limits {
@@ -62,16 +56,19 @@ EOF
     value = var.project
   }
 
-  tags {
-    key   = "create-alarms"
-    value = "false"
-  }
-
   update_policy {
     should_roll = true
 
     roll_config {
-      batch_size_percentage = 25
+      batch_size_percentage = 33
+    }
+  }
+
+  scheduled_task {
+    tasks {
+      is_enabled      = true
+      cron_expression = "0 3 * * *"
+      task_type       = "clusterRoll"
     }
   }
 }
@@ -84,13 +81,7 @@ resource "spotinst_ocean_ecs_launch_spec" "ocean" {
 #!/bin/bash
 echo ECS_INSTANCE_ATTRIBUTES='{"lifecycle":"spot"}' >> /etc/ecs/ecs.config
 echo ECS_CLUSTER=${var.project}-${var.environment}-${var.family} >> /etc/ecs/ecs.config
-echo ECS_ENGINE_TASK_CLEANUP_WAIT_DURATION=1m >> /etc/ecs/ecs.config
-echo ECS_NUM_IMAGES_DELETE_PER_CYCLE=100 >> /etc/ecs/ecs.config
-echo ECS_RESERVED_MEMORY=512 >> /etc/ecs/ecs.config
-echo ECS_AVAILABLE_LOGGING_DRIVERS=[\"json-file\", \"none\", \"gelf\", \"awslogs\", \"fluentd\"] >> /etc/ecs/ecs.config
-echo ECS_ENABLE_CONTAINER_METADATA=true >> /etc/ecs/ecs.config
-echo ECS_ENABLE_UNTRACKED_IMAGE_CLEANUP=true >> /etc/ecs/ecs.config
-echo ECS_ENABLE_SPOT_INSTANCE_DRAINING=true >> /etc/ecs/ecs.config
+echo ECS_AVAILABLE_LOGGING_DRIVERS=[\"none\", \"awslogs\", \"fluentd\"] >> /etc/ecs/ecs.config
 EOF
 
   iam_instance_profile = aws_iam_instance_profile.ec2.id
@@ -113,15 +104,9 @@ resource "spotinst_elastigroup_aws" "main" {
 
   user_data = <<EOF
 #!/bin/bash
-echo ECS_INSTANCE_ATTRIBUTES='{"lifecycle":"od"}' >> /etc/ecs/ecs.config
+echo ECS_INSTANCE_ATTRIBUTES='{"lifecycle":"spot"}' >> /etc/ecs/ecs.config
 echo ECS_CLUSTER=${var.project}-${var.environment}-${var.family} >> /etc/ecs/ecs.config
-echo ECS_ENGINE_TASK_CLEANUP_WAIT_DURATION=1m >> /etc/ecs/ecs.config
-echo ECS_NUM_IMAGES_DELETE_PER_CYCLE=100 >> /etc/ecs/ecs.config
-echo ECS_RESERVED_MEMORY=512 >> /etc/ecs/ecs.config
-echo ECS_AVAILABLE_LOGGING_DRIVERS=[\"json-file\", \"none\", \"gelf\", \"awslogs\", \"fluentd\"] >> /etc/ecs/ecs.config
-echo ECS_ENABLE_CONTAINER_METADATA=true >> /etc/ecs/ecs.config
-echo ECS_ENABLE_UNTRACKED_IMAGE_CLEANUP=true >> /etc/ecs/ecs.config
-echo ECS_ENABLE_SPOT_INSTANCE_DRAINING=true >> /etc/ecs/ecs.config
+echo ECS_AVAILABLE_LOGGING_DRIVERS=[\"none\", \"awslogs\", \"fluentd\"] >> /etc/ecs/ecs.config
 EOF
 
   image_id             = data.aws_ssm_parameter.ami.value
@@ -170,11 +155,6 @@ EOF
   tags {
     key   = "Project"
     value = var.project
-  }
-
-  tags {
-    key   = "create-alarms"
-    value = "false"
   }
 
   update_policy {
