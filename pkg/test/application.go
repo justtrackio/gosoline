1fcf6bb384de04a17ddb755dbd67bba4efc6cd71
Bogdan Finn <bogdan@applike.info>
2020-11-27 20:03:16 +0100

    make all paths absolut;

--- pkg/test/application.go
+++ pkg/test/application.go
@@ -3,11 +3,16 @@ package test
 import (
 	"github.com/applike/gosoline/pkg/application"
 	"github.com/applike/gosoline/pkg/kernel"
+	"os"
+	"path/filepath"
 )
 
 func Application() kernel.Kernel {
+	ex, _ := os.Executable()
+	configFilePath := filepath.Join(filepath.Dir(ex), "config.dist.yml")
+
 	options := []application.Option{
-		application.WithConfigFile("./config.dist.yml", "yml"),
+		application.WithConfigFile(configFilePath, "yml"),
 	}
 
 	return application.New(options...)
