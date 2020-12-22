9a0696c68b2019feb227b95e5683fc530404138a
Anselm Jonas Scholl <aj@justdice.io>
2020-12-22 16:08:30 +0100

    tf: add scheduled autoscaling for global secondary indices;

--- deployments/terraform/modules/dynamodb-autoscaler/schedule.tf
+++ deployments/terraform/modules/dynamodb-autoscaler/schedule.tf
@@ -14,6 +14,22 @@ resource "aws_appautoscaling_scheduled_action" "table_read_start" {
   }
 }
 
+resource "aws_appautoscaling_scheduled_action" "index_read_start" {
+  count = var.enabled_global_secondary_index ? length(var.autoscaling_schedule_table_read_start) * length(var.dynamodb_indexes) : 0
+
+  name               = "dynamodb-${var.dynamodb_table_name}-${count.index % length(var.dynamodb_indexes)}-read-${element(var.dynamodb_indexes, count.index / length(var.dynamodb_indexes))}-schedule-start"
+  service_namespace  = aws_appautoscaling_target.read_target_index[count.index / length(var.dynamodb_indexes)].service_namespace
+  resource_id        = aws_appautoscaling_target.read_target_index[count.index / length(var.dynamodb_indexes)].resource_id
+  scalable_dimension = aws_appautoscaling_target.read_target_index[count.index / length(var.dynamodb_indexes)].scalable_dimension
+
+  schedule = "cron(${var.autoscaling_schedule_table_read_start[count.index % length(var.dynamodb_indexes)].cron})"
+
+  scalable_target_action {
+    min_capacity = var.autoscaling_schedule_table_read_start[count.index % length(var.dynamodb_indexes)].min_capacity
+    max_capacity = var.autoscaling_schedule_table_read_start[count.index % length(var.dynamodb_indexes)].max_capacity
+  }
+}
+
 resource "aws_appautoscaling_scheduled_action" "table_read_stop" {
   count = length(var.autoscaling_schedule_table_read_stop)
 
@@ -27,6 +43,22 @@ resource "aws_appautoscaling_scheduled_action" "table_read_stop" {
   scalable_target_action {
     min_capacity = var.autoscaling_schedule_table_read_stop[count.index].min_capacity
     max_capacity = var.autoscaling_schedule_table_read_stop[count.index].max_capacity
+  }
+}
+
+resource "aws_appautoscaling_scheduled_action" "index_read_stop" {
+  count = var.enabled_global_secondary_index ? length(var.autoscaling_schedule_table_read_stop) * length(var.dynamodb_indexes) : 0
+
+  name               = "dynamodb-${var.dynamodb_table_name}-${count.index % length(var.dynamodb_indexes)}-read-${element(var.dynamodb_indexes, count.index / length(var.dynamodb_indexes))}-schedule-stop"
+  service_namespace  = aws_appautoscaling_target.read_target_index[count.index / length(var.dynamodb_indexes)].service_namespace
+  resource_id        = aws_appautoscaling_target.read_target_index[count.index / length(var.dynamodb_indexes)].resource_id
+  scalable_dimension = aws_appautoscaling_target.read_target_index[count.index / length(var.dynamodb_indexes)].scalable_dimension
+
+  schedule = "cron(${var.autoscaling_schedule_table_read_stop[count.index % length(var.dynamodb_indexes)].cron})"
+
+  scalable_target_action {
+    min_capacity = var.autoscaling_schedule_table_read_stop[count.index % length(var.dynamodb_indexes)].min_capacity
+    max_capacity = var.autoscaling_schedule_table_read_stop[count.index % length(var.dynamodb_indexes)].max_capacity
   }
 }
 
@@ -46,6 +78,22 @@ resource "aws_appautoscaling_scheduled_action" "table_write_start" {
   }
 }
 
+resource "aws_appautoscaling_scheduled_action" "index_write_start" {
+  count = var.enabled_global_secondary_index ? length(var.autoscaling_schedule_table_write_start) * length(var.dynamodb_indexes) : 0
+
+  name               = "dynamodb-${var.dynamodb_table_name}-${count.index % length(var.dynamodb_indexes)}-write-${element(var.dynamodb_indexes, count.index / length(var.dynamodb_indexes))}-schedule-start"
+  service_namespace  = aws_appautoscaling_target.write_target_index[count.index / length(var.dynamodb_indexes)].service_namespace
+  resource_id        = aws_appautoscaling_target.write_target_index[count.index / length(var.dynamodb_indexes)].resource_id
+  scalable_dimension = aws_appautoscaling_target.write_target_index[count.index / length(var.dynamodb_indexes)].scalable_dimension
+
+  schedule = "cron(${var.autoscaling_schedule_table_write_start[count.index % length(var.dynamodb_indexes)].cron})"
+
+  scalable_target_action {
+    min_capacity = var.autoscaling_schedule_table_write_start[count.index % length(var.dynamodb_indexes)].min_capacity
+    max_capacity = var.autoscaling_schedule_table_write_start[count.index % length(var.dynamodb_indexes)].max_capacity
+  }
+}
+
 resource "aws_appautoscaling_scheduled_action" "table_write_stop" {
   count = length(var.autoscaling_schedule_table_write_stop)
 
@@ -59,5 +107,21 @@ resource "aws_appautoscaling_scheduled_action" "table_write_stop" {
   scalable_target_action {
     min_capacity = var.autoscaling_schedule_table_write_stop[count.index].min_capacity
     max_capacity = var.autoscaling_schedule_table_write_stop[count.index].max_capacity
+  }
+}
+
+resource "aws_appautoscaling_scheduled_action" "index_write_stop" {
+  count = var.enabled_global_secondary_index ? length(var.autoscaling_schedule_table_write_stop) * length(var.dynamodb_indexes) : 0
+
+  name               = "dynamodb-${var.dynamodb_table_name}-${count.index % length(var.dynamodb_indexes)}-write-${element(var.dynamodb_indexes, count.index / length(var.dynamodb_indexes))}-schedule-stop"
+  service_namespace  = aws_appautoscaling_target.write_target_index[count.index / length(var.dynamodb_indexes)].service_namespace
+  resource_id        = aws_appautoscaling_target.write_target_index[count.index / length(var.dynamodb_indexes)].resource_id
+  scalable_dimension = aws_appautoscaling_target.write_target_index[count.index / length(var.dynamodb_indexes)].scalable_dimension
+
+  schedule = "cron(${var.autoscaling_schedule_table_write_stop[count.index % length(var.dynamodb_indexes)].cron})"
+
+  scalable_target_action {
+    min_capacity = var.autoscaling_schedule_table_write_stop[count.index % length(var.dynamodb_indexes)].min_capacity
+    max_capacity = var.autoscaling_schedule_table_write_stop[count.index % length(var.dynamodb_indexes)].max_capacity
   }
 }
