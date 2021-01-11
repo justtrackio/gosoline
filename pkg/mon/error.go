bd35ca2fd0129a8b4247e61f145705740da6702b
Anselm Jonas Scholl <aj@justdice.io>
2021-01-11 18:16:56 +0100

    mon/mocks: add common error detection to logger mock;
    Added `NewLoggerMockedLevel` to allow specifying the maximum log level
    you expect to be logged. A common use case is a test where a component
    might normally swallow an error, but you need an easy way to ensure no
    such error was thrown and logged by your test. Additionally the logger
    mock now checks the format of the message and, if formatting marks are
    detected, the use of Infof/Warnf/... is enforced. If no such marks are
    detected, Info/Warn/... should be used instead.

--- pkg/mon/error.go
+++ pkg/mon/error.go
@@ -3,10 +3,8 @@ package mon
 import "fmt"
 
 func WrapErrorAndLog(logger Logger, err error, msg string, args ...interface{}) error {
-	logger.Errorf(err, msg, args...)
-
 	errMsg := fmt.Sprintf(msg, args...)
-	err = fmt.Errorf("%s: %w", errMsg, err)
+	logger.Error(err, errMsg)
 
-	return err
+	return fmt.Errorf("%s: %w", errMsg, err)
 }
--- pkg/mon/mocks/factory.go
+++ pkg/mon/mocks/factory.go
@@ -1,30 +1,100 @@
 package mocks
 
-import "github.com/stretchr/testify/mock"
+import (
+	"fmt"
+	"github.com/applike/gosoline/pkg/mon"
+	"github.com/stretchr/testify/mock"
+)
 
-func NewLoggerMock(methods ...string) *Logger {
+func NewLoggerMock() *Logger {
 	logger := new(Logger)
 
-	for _, m := range methods {
-		logger.On(m, mock.Anything).Return(logger)
-		logger.On(m, mock.Anything, mock.Anything).Return(logger)
-		logger.On(m, mock.Anything, mock.Anything, mock.Anything).Return(logger)
-		logger.On(m, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(logger)
-		logger.On(m, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(logger)
-		logger.On(m, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(logger)
-	}
+	logger.On("WithChannel", mock.AnythingOfType("string")).Return(logger).Maybe()
+	logger.On("WithContext", mock.Anything).Return(logger).Maybe()
+	logger.On("WithFields", mock.AnythingOfType("mon.Fields")).Return(logger).Maybe()
 
 	return logger
 }
 
 func NewLoggerMockedAll() *Logger {
-	return NewLoggerMock("Debug", "Debugf", "Info", "Infof", "Warn", "Warnf", "Error", "Errorf", "WithChannel", "WithContext", "WithFields")
+	return NewLoggerMockedUntilLevel(mon.Panic)
+}
+
+// return a logger mocked up to the given log level. All other calls will cause an error and fail the test.
+func NewLoggerMockedUntilLevel(level string) *Logger {
+	logger := NewLoggerMock()
+
+	levelMap := map[string]int{
+		mon.Trace: 0,
+		mon.Debug: 1,
+		mon.Info:  2,
+		mon.Warn:  3,
+		mon.Error: 4,
+		mon.Fatal: 5,
+		mon.Panic: 6,
+	}
+
+	levelIndex, ok := levelMap[level]
+
+	if !ok {
+		panic(fmt.Errorf("failed to find level to mock: %s", level))
+	}
+
+	mockLoggerMethod(logger, "Debug", "Debugf", mon.Debug, levelIndex >= levelMap[mon.Debug])
+	mockLoggerMethod(logger, "Info", "Infof", mon.Info, levelIndex >= levelMap[mon.Info])
+	mockLoggerMethod(logger, "Warn", "Warnf", mon.Warn, levelIndex >= levelMap[mon.Warn])
+	mockLoggerMethod(logger, "Error", "Errorf", mon.Error, levelIndex >= levelMap[mon.Error])
+	mockLoggerMethod(logger, "Fatal", "Fatalf", mon.Fatal, levelIndex >= levelMap[mon.Fatal])
+	mockLoggerMethod(logger, "Panic", "Panicf", mon.Panic, levelIndex >= levelMap[mon.Panic])
+
+	return logger
+}
+
+func inspectLogFunction(level string, withFormat bool, allowed bool) func(args mock.Arguments) {
+	return func(args mock.Arguments) {
+		if level == mon.Panic || level == mon.Fatal {
+			err := args.Get(0).(error)
+			var msg string
+			if withFormat {
+				msg = fmt.Sprintf(args.Get(1).(string), args[2:]...)
+			} else {
+				msg = fmt.Sprint(args[1:])
+			}
+			// we have to stop the test, these methods never return
+			panic(fmt.Errorf("panic or fatal logging method called with error '%w' and message '%s'", err, msg))
+		}
+
+		// ensure we use formatting markers exactly when using a method with formatting
+		var msg string
+		if level == mon.Error {
+			msg = fmt.Sprint(args.Get(1))
+		} else if len(args) > 0 {
+			msg = fmt.Sprint(args.Get(0))
+		}
+
+		if !allowed {
+			panic(fmt.Errorf("invalid log message '%s'. Logs of level %s are not allowed", msg, level))
+		}
+	}
+}
+
+func mockLoggerMethod(logger *Logger, method string, methodWithFormat string, level string, allowed bool) {
+	anythings := make(mock.Arguments, 0)
+	f := inspectLogFunction(level, false, allowed)
+	fWithFormat := inspectLogFunction(level, true, allowed)
+
+	for i := 0; i < 10; i++ {
+		anythings = append(anythings, mock.Anything)
+		logger.On(method, anythings...).Run(f).Return(logger).Maybe()
+		logger.On(methodWithFormat, anythings...).Run(fWithFormat).Return(logger).Maybe()
+	}
 }
 
 func NewMetricWriterMockedAll() *MetricWriter {
 	mw := new(MetricWriter)
-	mw.On("Write", mock.Anything).Return()
-	mw.On("WriteOne", mock.Anything).Return()
+	mw.On("GetPriority").Return(mon.PriorityLow).Maybe()
+	mw.On("Write", mock.AnythingOfType("mon.MetricData")).Return().Maybe()
+	mw.On("WriteOne", mock.AnythingOfType("*mon.MetricDatum")).Return().Maybe()
 
 	return mw
 }
