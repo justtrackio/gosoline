2ad0ff6e286f49bd111d6bbbf3310fc6934f8a96
Sebastian Struß <struss@justdice.io>
2021-05-20 11:13:39 +0200

    deployment: make ecr lifecycle policy optional;

---
+++ deployments/terraform/modules/ecr/output.tf
@@ -0,0 +1,11 @@
+output "repository_arn" {
+  value = aws_ecr_repository.main.arn
+}
+
+output "repository_id" {
+  value = aws_ecr_repository.main.id
+}
+
+output "repository_name" {
+  value = aws_ecr_repository.main.name
+}
--- deployments/terraform/modules/ecr/variables.tf
+++ deployments/terraform/modules/ecr/variables.tf
@@ -6,3 +6,7 @@ variable "primary_tag" {
   type    = string
   default = "master"
 }
+
+variable "use_default_lifecycle_policy" {
+  default = 1
+}
