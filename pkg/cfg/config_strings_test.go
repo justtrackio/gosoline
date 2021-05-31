979187012dca33fd47188dc919370b0c1e023017
Anselm Jonas Scholl <aj@justdice.io>
2021-05-31 16:10:27 +0200

    cfg, mapx: teach mapx to cast newtypes and descend into slices and maps;
    If you had a `type MyString string` and then tried to use `MyString` in
    your config types, this would fail because string was not convertible to
    it. Now we also allow conversions for such types to be performed as well
    as converting `[]string` to `[]MyString`, `map[string]string` to
    `map[MyString]MyString`, and so on recursively.

---
+++ pkg/cfg/config_strings_test.go
@@ -0,0 +1,96 @@
+package cfg_test
+
+import (
+	"github.com/applike/gosoline/pkg/cfg"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type Encoding string
+type Compression string
+
+const (
+	EncodingJson    Encoding    = "json"
+	EncodingHtml    Encoding    = "html"
+	EncodingPng     Encoding    = "png"
+	CompressionGzip Compression = "gzip"
+	CompressionNone Compression = "none"
+)
+
+type TestSettings struct {
+	Encoding                  Encoding                                  `cfg:"encoding"`
+	FallbackEncoding          Encoding                                  `cfg:"fallback_encoding" default:"json"`
+	Compression               Compression                               `cfg:"compression" validate:"oneof=gzip none"`
+	FallbackCompression       Compression                               `cfg:"fallback_compression" default:"none" validate:"oneof=gzip none"`
+	SupportedCompression      []Compression                             `cfg:"supported_compression" validate:"min=1"`
+	EncodingCompression       map[Encoding]Compression                  `cfg:"encoding_compression"`
+	SupportedCompressions     map[Encoding][]Compression                `cfg:"supported_compressions"`
+	AllEncodings              []Encoding                                `cfg:"all_encodings"`
+	OkayIDontKnowANameForThis map[Encoding][]map[Encoding][]Compression `cfg:"name_this"`
+}
+
+func TestConfigStringsTest(t *testing.T) {
+	config := cfg.New()
+	err := config.Option(cfg.WithConfigFile("./testdata/config.strings.test.yml", "yml"))
+	assert.NoError(t, err)
+
+	settings := &TestSettings{}
+	config.UnmarshalKey("test", settings)
+
+	assert.Equal(t, &TestSettings{
+		Encoding:            EncodingJson,
+		FallbackEncoding:    EncodingJson,
+		Compression:         CompressionGzip,
+		FallbackCompression: CompressionNone,
+		SupportedCompression: []Compression{
+			CompressionGzip,
+			CompressionNone,
+		},
+		EncodingCompression: map[Encoding]Compression{
+			EncodingJson: CompressionGzip,
+			EncodingHtml: CompressionGzip,
+			EncodingPng:  CompressionNone,
+		},
+		SupportedCompressions: map[Encoding][]Compression{
+			EncodingJson: {CompressionGzip, CompressionNone},
+			EncodingHtml: {CompressionGzip},
+			EncodingPng:  {CompressionNone},
+		},
+		AllEncodings: []Encoding{
+			EncodingJson,
+			EncodingHtml,
+			EncodingPng,
+		},
+		OkayIDontKnowANameForThis: map[Encoding][]map[Encoding][]Compression{
+			EncodingJson: {
+				{
+					EncodingHtml: {
+						CompressionGzip,
+						CompressionNone,
+					},
+					EncodingPng: {
+						CompressionNone,
+					},
+				},
+				{
+					EncodingHtml: {},
+					EncodingJson: {
+						CompressionNone,
+					},
+				},
+			},
+			EncodingHtml: {
+				{
+					EncodingJson: {},
+				},
+				{
+					EncodingHtml: {},
+					EncodingJson: {
+						CompressionGzip,
+						CompressionGzip,
+					},
+				},
+			},
+		},
+	}, settings)
+}
