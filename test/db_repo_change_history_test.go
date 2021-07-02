f3c6d2d949994435228ad6c7e8811a57d4a7d209
Mohamed Tadj Eddine Gougam <mohamed@justDice.io>
2021-07-02 12:17:18 +0200

    fix change history integration tests;

--- test/db_repo_change_history_test.go
+++ test/db_repo_change_history_test.go
@@ -13,12 +13,91 @@ import (
 	"testing"
 )
 
+type TestModel1 struct {
+	db_repo.Model
+	Name *string
+}
+
+type TestModel1HistoryEntry struct {
+	db_repo.ChangeHistoryModel
+	TestModel1
+}
+
+var TestModel1Metadata = db_repo.Metadata{
+	ModelId: mdl.ModelId{
+		Application: "application",
+		Name:        "testModel1",
+	},
+	TableName:  "test_model1",
+	PrimaryKey: "test_model1.id",
+	Mappings: db_repo.FieldMappings{
+		"testModel1.id":   db_repo.NewFieldMapping("test_model1.id"),
+		"testModel1.name": db_repo.NewFieldMapping("test_model1.name"),
+	},
+}
+
+var TestHistoryModel1Metadata = db_repo.Metadata{
+	ModelId: mdl.ModelId{
+		Application: "application",
+		Name:        "testModel1HistoryEntry",
+	},
+	TableName:  "test_model1_history_entries",
+	PrimaryKey: "test_model1_history_entries.id",
+	Mappings: db_repo.FieldMappings{
+		"testModel1HistoryEntry.id":   db_repo.NewFieldMapping("test_model1_history_entries.id"),
+		"testModel1HistoryEntry.name": db_repo.NewFieldMapping("test_model1_history_entries.name"),
+	},
+}
+
+type TestModel2 struct {
+	db_repo.Model
+	Name         *string
+	Foo          *string
+	ChangeAuthor *string
+}
+
+type TestModel2HistoryEntry struct {
+	db_repo.ChangeHistoryModel
+	TestModel2
+}
+
+var TestModel2Metadata = db_repo.Metadata{
+	ModelId: mdl.ModelId{
+		Application: "application",
+		Name:        "testModel2",
+	},
+	TableName:  "test_model2",
+	PrimaryKey: "test_model2.id",
+	Mappings: db_repo.FieldMappings{
+		"testModel2.id":   db_repo.NewFieldMapping("test_model2.id"),
+		"testModel2.name": db_repo.NewFieldMapping("test_model2.name"),
+		"testModel2.foo": db_repo.NewFieldMapping("test_model2.foo"),
+		"testModel2.changeAuthor": db_repo.NewFieldMapping("test_model2.change_author"),
+	},
+}
+
+var TestHistoryModel2Metadata = db_repo.Metadata{
+	ModelId: mdl.ModelId{
+		Application: "application",
+		Name:        "testModel2HistoryEntry",
+	},
+	TableName:  "test_model2_history_entries",
+	PrimaryKey: "test_model2_history_entries.id",
+	Mappings: db_repo.FieldMappings{
+		"testModel2HistoryEntry.id":   db_repo.NewFieldMapping("test_model2_history_entries.id"),
+		"testModel2HistoryEntry.name": db_repo.NewFieldMapping("test_model2_history_entries.name"),
+		"testModel2HistoryEntry.foo": db_repo.NewFieldMapping("test_model2_history_entries.foo"),
+		"testModel2HistoryEntry.changeAuthor": db_repo.NewFieldMapping("test_model2_history_entries.change_author"),
+	},
+}
+
 type DbRepoChangeHistoryTestSuite struct {
 	suite.Suite
 	logger mon.Logger
 	config cfg.Config
 	mocks  *test.Mocks
-	repo   db_repo.Repository
+	modelRepo   db_repo.Repository
+	modelHistoryRepo   db_repo.Repository
 }
 
 func TestDbChangelogTestSuite(t *testing.T) {
@@ -47,9 +126,6 @@ func (s *DbRepoChangeHistoryTestSuite) SetupSuite() {
 
 	s.config = config
 	s.logger = mon.NewLogger()
-
-	s.repo, err = db_repo.New(s.config, s.logger, db_repo.Settings{})
-	s.NoError(err)
 }
 
 func (s *DbRepoChangeHistoryTestSuite) TearDownSuite() {
@@ -57,36 +133,37 @@ func (s *DbRepoChangeHistoryTestSuite) TearDownSuite() {
 }
 
 func (s *DbRepoChangeHistoryTestSuite) TestChangeHistoryMigration_Migrate_CreateTable() {
+	var err error
 
-	type TestModel1 struct {
-		db_repo.Model
-		Name *string
-	}
+	s.modelRepo, err = db_repo.New(s.config, s.logger, db_repo.Settings{
+		Metadata: TestModel1Metadata,
+	})
+	s.NoError(err)
 
-	type TestModel1HistoryEntry struct {
-		db_repo.ChangeHistoryModel
-		TestModel1
-	}
+	s.modelHistoryRepo, err = db_repo.New(s.config, s.logger, db_repo.Settings{
+		Metadata: TestHistoryModel1Metadata,
+	})
+	s.NoError(err)
 
-	err := db_repo.MigrateChangeHistory(s.config, s.logger, &TestModel1{})
+	err = db_repo.MigrateChangeHistory(s.config, s.logger, &TestModel1{})
 	s.NoError(err)
 
 	model := &TestModel1{
 		Name: mdl.String("name1"),
 	}
 
-	err = s.repo.Create(context.Background(), model)
+	err = s.modelRepo.Create(context.Background(), model)
 	s.NoError(err)
 
 	model.Name = mdl.String("name2")
-	err = s.repo.Update(context.Background(), model)
+	err = s.modelRepo.Update(context.Background(), model)
 	s.NoError(err)
 
-	err = s.repo.Delete(context.Background(), model)
+	err = s.modelRepo.Delete(context.Background(), model)
 	s.NoError(err)
 
 	entries := make([]*TestModel1HistoryEntry, 0)
-	err = s.repo.Query(context.Background(), &db_repo.QueryBuilder{}, &entries)
+	err = s.modelHistoryRepo.Query(context.Background(), &db_repo.QueryBuilder{}, &entries)
 	s.NoError(err)
 	s.Equal(3, len(entries), "expected 3 change history entries")
 
@@ -104,20 +181,19 @@ func (s *DbRepoChangeHistoryTestSuite) TestChangeHistoryMigration_Migrate_Create
 }
 
 func (s *DbRepoChangeHistoryTestSuite) TestChangeHistoryMigration_Migrate_UpdateTable() {
+	var err error
 
-	type TestModel2 struct {
-		db_repo.Model
-		Name         *string
-		Foo          *string
-		ChangeAuthor *string
-	}
+	s.modelRepo, err = db_repo.New(s.config, s.logger, db_repo.Settings{
+		Metadata: TestModel2Metadata,
+	})
+	s.NoError(err)
 
-	type TestModel2HistoryEntry struct {
-		db_repo.ChangeHistoryModel
-		TestModel2
-	}
+	s.modelHistoryRepo, err = db_repo.New(s.config, s.logger, db_repo.Settings{
+		Metadata: TestHistoryModel2Metadata,
+	})
+	s.NoError(err)
 
-	err := db_repo.MigrateChangeHistory(s.config, s.logger, &TestModel2{})
+	err = db_repo.MigrateChangeHistory(s.config, s.logger, &TestModel2{})
 	s.NoError(err)
 
 	model := &TestModel2{
@@ -126,18 +202,18 @@ func (s *DbRepoChangeHistoryTestSuite) TestChangeHistoryMigration_Migrate_Update
 		ChangeAuthor: mdl.String("john@example.com"),
 	}
 
-	err = s.repo.Create(context.Background(), model)
+	err = s.modelRepo.Create(context.Background(), model)
 	s.NoError(err)
 
 	model.Foo = mdl.String("foo2")
-	err = s.repo.Update(context.Background(), model)
+	err = s.modelRepo.Update(context.Background(), model)
 	s.NoError(err)
 
-	err = s.repo.Delete(context.Background(), model)
+	err = s.modelRepo.Delete(context.Background(), model)
 	s.NoError(err)
 
 	entries := make([]*TestModel2HistoryEntry, 0)
-	err = s.repo.Query(context.Background(), &db_repo.QueryBuilder{}, &entries)
+	err = s.modelHistoryRepo.Query(context.Background(), &db_repo.QueryBuilder{}, &entries)
 	s.NoError(err)
 	s.Equal(3, len(entries), "expected 3 change history entries")
 
