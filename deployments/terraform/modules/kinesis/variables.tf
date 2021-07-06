324fb199103dac92be619531f228958ab9d457c5
erdal <erdal@applike.info>
2021-07-06 20:19:18 +0200

    tf: monitor kinesis get and put records success metrics;

--- deployments/terraform/modules/kinesis/variables.tf
+++ deployments/terraform/modules/kinesis/variables.tf
@@ -18,9 +18,17 @@ variable "alarm_datapoints_to_alarm" {
   type    = number
   default = 4
 }
+variable "alarm_put_records_datapoints_to_alarm" {
+  type    = number
+  default = 6
+}
 variable "alarm_limit_threshold_percentage" {
   type    = number
   default = 80
+}
+variable "alarm_records_success_threshold" {
+  type    = number
+  default = 0.99
 }
 variable "alarm_iterator_threshold_age_milliseconds" {
   type    = number
