991c4e8f0f77ae117b8f11a93e5802365a0ccf89
erdal <erdal@applike.info>
2021-08-03 16:03:08 +0200

    sns: make sns.Message writable

--- pkg/sns/message.go
+++ pkg/sns/message.go
@@ -1,8 +1,23 @@
 package sns
 
+import "github.com/applike/gosoline/pkg/encoding/json"
+
 type Message struct {
 	Type      string
 	TopicArn  string
 	MessageId string
 	Message   string
+}
+
+func (m Message) MarshalToBytes() ([]byte, error) {
+	return json.Marshal(m)
+}
+
+func (m Message) MarshalToString() (string, error) {
+	bytes, err := json.Marshal(m)
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes), nil
 }
