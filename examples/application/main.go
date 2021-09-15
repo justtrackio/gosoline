116b2081ee7a34da79a0deea460377d9e62b2a08
Jan K <jankamieth@gmail.com>
2021-09-15 08:41:46 +0200

    readme: added introduction and quickstart section;

---
+++ examples/application/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+
+	"github.com/justtrackio/gosoline/pkg/application"
+	"github.com/justtrackio/gosoline/pkg/cfg"
+	"github.com/justtrackio/gosoline/pkg/kernel"
+	"github.com/justtrackio/gosoline/pkg/log"
+)
+
+func main() {
+	app := application.Default()
+	app.Add("hello-world", NewHelloWorldModule)
+	app.Run()
+}
+
+func NewHelloWorldModule(ctx context.Context, config cfg.Config, logger log.Logger) (kernel.Module, error) {
+	return &HelloWorldModule{
+		logger: logger.WithChannel("hello-world"),
+	}, nil
+}
+
+type HelloWorldModule struct {
+	logger log.Logger
+}
+
+func (h HelloWorldModule) Run(ctx context.Context) error {
+	h.logger.Info("Hello World")
+	return nil
+}
