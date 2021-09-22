0f566fa01ea1da2ec3af7d2d0751f84f5e12ed37
Anselm Jonas Scholl <aj@justdice.io>
2021-09-22 09:47:02 +0200

    lambda, application: The lambda module now applies the config post-processors upon startup. There is also a new method `application.RunModule` to quickly run a single module;

--- pkg/lambda/lambda.go
+++ pkg/lambda/lambda.go
@@ -1,19 +1,20 @@
 package lambda
 
 import (
+	"context"
 	"os"
-	"strings"
 
 	awsLambda "github.com/aws/aws-lambda-go/lambda"
+	"github.com/justtrackio/gosoline/pkg/appctx"
 	"github.com/justtrackio/gosoline/pkg/cfg"
 	"github.com/justtrackio/gosoline/pkg/clock"
 	"github.com/justtrackio/gosoline/pkg/log"
 	"github.com/justtrackio/gosoline/pkg/stream"
 )
 
-type HandlerFactory func(config cfg.Config, logger log.Logger) (interface{}, error)
+type HandlerFactory func(ctx context.Context, config cfg.Config, logger log.Logger) (interface{}, error)
 
-func Start(handlerFactory HandlerFactory, defaultConfig ...map[string]interface{}) {
+func Start(handlerFactory HandlerFactory, configOptions ...cfg.Option) {
 	clock.WithUseUTC(true)
 
 	logHandler := log.NewHandlerIoWriter(log.LevelInfo, []string{}, log.FormatterConsole, "", os.Stdout)
@@ -26,35 +27,46 @@ func Start(handlerFactory HandlerFactory, defaultConfig ...map[string]interface{
 
 	if err := logger.Option(loggerOptions...); err != nil {
 		logger.Error("failed to apply logger options: %w", err)
+
 		os.Exit(1)
 	}
 
 	// configure and create config
-	configOptions := []cfg.Option{
-		cfg.WithEnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_")),
+	mergedConfigOptions := append([]cfg.Option{
+		cfg.WithEnvKeyReplacer(cfg.DefaultEnvKeyReplacer),
 		cfg.WithSanitizers(cfg.TimeSanitizer),
 		cfg.WithErrorHandlers(func(msg string, args ...interface{}) {
 			logger.Error(msg, args...)
 			os.Exit(1)
 		}),
-	}
-
-	for _, defaults := range defaultConfig {
-		configOptions = append(configOptions, cfg.WithConfigMap(defaults))
-	}
+	}, configOptions...)
 
 	config := cfg.New()
-	if err := config.Option(configOptions...); err != nil {
-		logger.Error("failed to apply logger options: %w", err)
+	if err := config.Option(mergedConfigOptions...); err != nil {
+		logger.Error("failed to apply config options: %w", err)
+
 		os.Exit(1)
+	}
+
+	if cfgPostProcessors, err := cfg.ApplyPostProcessors(config); err != nil {
+		logger.Error("can not apply post processor on config: %w", err)
+
+		os.Exit(1)
+	} else {
+		for name, priority := range cfgPostProcessors {
+			logger.Debug("applied priority %d config post processor '%s'", priority, name)
+		}
 	}
 
 	stream.AddDefaultEncodeHandler(log.NewMessageWithLoggingFieldsEncoder(config, logger))
 
+	ctx := appctx.WithContainer(context.Background())
+
 	// create handler function and give lambda control
-	lambdaHandler, err := handlerFactory(config, logger)
+	lambdaHandler, err := handlerFactory(ctx, config, logger)
 	if err != nil {
 		logger.Error("failed to create lambda handler: %w", err)
+
 		os.Exit(1)
 	}
 
