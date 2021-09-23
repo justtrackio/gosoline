6977ee79bdc030034ec2a319a1fb7660ae62b172
Marco Frömbgen <marco@justdice.io>
2021-09-23 09:51:54 +0200

    tf: remove-dreprecated-modules;

--- deployments/terraform/modules/ocean-eks/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  subnets            = var.subnets != null ? var.subnets : module.vpc.private_subnets
-  vpc_id             = var.vpc_id != null ? var.vpc_id : module.vpc.vpc_id
-  ami_id             = var.ami_id != null ? var.ami_id : module.eks.workers_default_ami_id
-  cluster_identifier = var.cluster_identifier != null ? var.cluster_identifier : module.eks.cluster_id
-}
--- deployments/terraform/modules/ocean-eks/scripts/bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -o xtrace
-/etc/eks/bootstrap.sh ${default_label}
-
-${nvme}
