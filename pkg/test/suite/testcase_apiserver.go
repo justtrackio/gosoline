34b4e7cd86d04a6f5ced817ac2750d45b9c520d8
Jan K <jankamieth@gmail.com>
2021-09-28 14:43:29 +0200

    test: more flexible api server testcase variant;

--- pkg/test/suite/testcase_apiserver.go
+++ pkg/test/suite/testcase_apiserver.go
@@ -3,7 +3,6 @@ package suite
 import (
 	"context"
 	"fmt"
-	"net/http"
 	"reflect"
 	"testing"
 
@@ -28,50 +27,8 @@ type TestingSuiteApiDefinitionsAware interface {
 	SetupApiDefinitions() apiserver.Definer
 }
 
-type ApiServerTestCase struct {
-	Method  string
-	Url     string
-	Headers map[string]string
-	Body    interface{}
-	// ExpectedStatusCode describes the status code the last response is required to have.
-	ExpectedStatusCode int
-	// ExpectedRedirectsToFollow describes the number of redirects we want to follow. It is an error if less redirects
-	// are performed. More redirects result in the last redirect being returned as the response instead (e.g., if it is
-	// to some external site or with a protocol different from HTTP(S) like intent://) and do not result in an error.
-	ExpectedRedirectsToFollow int
-	// ExpectedResult defines the *type* the final response should be parsed into. You can then access the unmarshalled
-	// response in response.Result().
-	ExpectedResult interface{}
-	// ExpectedErr is compared with the error returned by the HTTP request. Only the error messages have to match.
-	ExpectedErr error
-	// Assert allows you to provide an assertion function which can be passed to validate certain post conditions (like
-	// messages being written to the correct queues).
-	// It also allows to check that the response carries the correct response body, redirects to the correct
-	//	// location, or has the correct headers set. You don't need to validate the response status here, this is already
-	//	// performed automatically.
-	Assert func(response *resty.Response) error
-}
-
-func (c ApiServerTestCase) request(client *resty.Client) (*resty.Response, error) {
-	req := client.R()
-
-	if c.Headers != nil {
-		req.SetHeaders(c.Headers)
-	}
-
-	if c.Body != nil {
-		req.SetBody(c.Body)
-	}
-
-	if c.ExpectedResult != nil {
-		req.SetResult(c.ExpectedResult)
-	}
-
-	return req.Execute(c.Method, c.Url)
-}
-
 func isTestCaseApiServer(method reflect.Method) bool {
-	if method.Func.Type().NumIn() != 1 {
+	if method.Func.Type().NumIn() != 3 {
 		return false
 	}
 
@@ -79,14 +36,35 @@ func isTestCaseApiServer(method reflect.Method) bool {
 		return false
 	}
 
-	actualType0 := method.Func.Type().Out(0)
-	expectedType := reflect.TypeOf((*ApiServerTestCase)(nil))
-	expectedSliceType := reflect.TypeOf([]*ApiServerTestCase{})
+	actualType1 := method.Func.Type().In(1)
+	expectedType1 := reflect.TypeOf((*AppUnderTest)(nil)).Elem()
 
-	return actualType0 == expectedType || actualType0 == expectedSliceType
+	if actualType1 != expectedType1 {
+		return false
+	}
+
+	actualType2 := method.Func.Type().In(2)
+	expectedType2 := reflect.TypeOf(&resty.Client{})
+
+	return actualType2 == expectedType2
 }
 
 func buildTestCaseApiServer(suite TestingSuite, method reflect.Method) (testCaseRunner, error) {
+	return runTestCaseApiServer(suite, func(suite TestingSuite, app *appUnderTest, client *resty.Client) {
+		out := method.Func.Call([]reflect.Value{
+			reflect.ValueOf(suite),
+			reflect.ValueOf(app),
+			reflect.ValueOf(client),
+		})
+
+		err := out[0].Interface().(error)
+		if err != nil {
+			assert.FailNow(suite.T(), err.Error(), "testcase %s returned an unexpected error: %s", method.Name, err)
+		}
+	})
+}
+
+func runTestCaseApiServer(suite TestingSuite, testCase func(suite TestingSuite, app *appUnderTest, client *resty.Client)) (testCaseRunner, error) {
 	var ok bool
 	var apiDefinitionAware TestingSuiteApiDefinitionsAware
 	var server *apiserver.ApiServer
@@ -100,16 +78,6 @@ func buildTestCaseApiServer(suite TestingSuite, method reflect.Method) (testCase
 		// route definitions or test cases
 		suite.SetT(t)
 
-		out := method.Func.Call([]reflect.Value{reflect.ValueOf(suite)})[0].Interface()
-
-		var testCases []*ApiServerTestCase
-		if tc, ok := out.(*ApiServerTestCase); ok {
-			testCases = []*ApiServerTestCase{tc}
-		} else {
-			testCases = out.([]*ApiServerTestCase)
-		}
-
-		responses := make([]*resty.Response, len(testCases))
 		apiDefinitions := apiDefinitionAware.SetupApiDefinitions()
 
 		suiteOptions.appModules["api"] = func(ctx context.Context, config cfg.Config, logger log.Logger) (kernel.Module, error) {
@@ -136,49 +104,10 @@ func buildTestCaseApiServer(suite TestingSuite, method reflect.Method) (testCase
 				return
 			}
 
-			remainingRedirects := 0
 			url := fmt.Sprintf("http://127.0.0.1:%d", *port)
+			client := resty.New().SetHostURL(url)
 
-			client := resty.New().SetHostURL(url).SetRedirectPolicy(
-				resty.RedirectPolicyFunc(func(request *http.Request, _ []*http.Request) error {
-					if remainingRedirects <= 0 {
-						return http.ErrUseLastResponse
-					}
-
-					remainingRedirects--
-
-					return nil
-				}),
-			)
-
-			for i, tc := range testCases {
-				remainingRedirects = tc.ExpectedRedirectsToFollow
-				responses[i], err = tc.request(client)
-
-				assert.NotNil(t, responses[i], "there should be a response returned")
-
-				if responses[i] != nil {
-					assert.Equal(t, tc.ExpectedStatusCode, responses[i].StatusCode(), "response status code should match")
-					assert.Equalf(t, 0, remainingRedirects, "expected %d redirects, but only %d redirects where performed", tc.ExpectedRedirectsToFollow, tc.ExpectedRedirectsToFollow-remainingRedirects)
-				}
-
-				if tc.ExpectedErr == nil {
-					assert.NoError(t, err)
-				} else {
-					assert.EqualError(t, err, tc.ExpectedErr.Error())
-				}
-			}
-
-			app.Stop()
-			app.WaitDone()
-
-			for i, tc := range testCases {
-				if tc.Assert != nil {
-					if err := tc.Assert(responses[i]); err != nil {
-						assert.FailNow(t, err.Error(), "there should be no error on assert")
-					}
-				}
-			}
+			testCase(suite, app, client)
 		})
 	}, nil
 }
