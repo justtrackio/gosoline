01bea10f1a5ed2fd34acf33716825a364fc687d0
Marco Frömbgen <marco.froembgen@gmail.com>
2021-10-06 13:26:24 +0200

    tf: remove-dreprecated-modules; (#592)

--- deployments/terraform/modules/ecs_cluster_ocean/data.tf
+++
@@ -1,3 +0,0 @@
-data "aws_ssm_parameter" "ami" {
-  name = "/mcoins/packer/ecs/ami"
-}
--- deployments/terraform/modules/ecs_container_definition/output.tf
+++
@@ -1,9 +0,0 @@
-output "json" {
-  description = "JSON encoded list of container definitions for use with other terraform resources such as aws_ecs_task_definition"
-  value       = "[${local.json_map}]"
-}
-
-output "json_map" {
-  description = "JSON encoded container definitions for use with other terraform resources such as aws_ecs_task_definition"
-  value       = local.json_map
-}
