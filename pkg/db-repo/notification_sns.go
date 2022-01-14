e8221cd30e79cddfbdeb4da1ae13d9412015594e
Jan K <jankamieth@gmail.com>
2022-01-14 14:17:29 +0100

    db-repo: fix missing parts in modelId;

--- pkg/db-repo/notification_sns.go
+++ pkg/db-repo/notification_sns.go
@@ -11,17 +11,16 @@ import (
 )
 
 func NewSnsNotifier(ctx context.Context, config cfg.Config, logger log.Logger, modelId mdl.ModelId, version int, transformer mdl.TransformerResolver) (*baseNotifier, error) {
-	appId := cfg.AppId{
-		Project:     modelId.Project,
-		Environment: modelId.Environment,
-		Family:      modelId.Family,
-		Application: modelId.Application,
-	}
-	appId.PadFromConfig(config)
+	modelId.PadFromConfig(config)
 
 	output, err := stream.NewSnsOutput(ctx, config, logger, &stream.SnsOutputSettings{
 		TopicId: modelId.Name,
-		AppId:   appId,
+		AppId: cfg.AppId{
+			Project:     modelId.Project,
+			Environment: modelId.Environment,
+			Family:      modelId.Family,
+			Application: modelId.Application,
+		},
 	})
 	if err != nil {
 		return nil, fmt.Errorf("can not create sns output: %w", err)
