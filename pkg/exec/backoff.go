94205a8dc4d53c25c364d251776796aabc2e5050
Jan K <jankamieth@gmail.com>
2022-02-08 11:21:14 +0100

    cloud/aws/kinesis: using backoff delay for retrying failed written records;

---
+++ pkg/exec/backoff.go
@@ -0,0 +1,12 @@
+package exec
+
+import "github.com/cenkalti/backoff/v4"
+
+func NewExponentialBackOff(settings *BackoffSettings) *backoff.ExponentialBackOff {
+	backoffConfig := backoff.NewExponentialBackOff()
+	backoffConfig.InitialInterval = settings.InitialInterval
+	backoffConfig.MaxInterval = settings.MaxInterval
+	backoffConfig.MaxElapsedTime = settings.MaxElapsedTime
+
+	return backoffConfig
+}
