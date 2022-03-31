8fd28c1085806fac791dde03f87fcb2e4bb7a0b2
Jan K <jankamieth@gmail.com>
2022-03-31 10:20:31 +0200

    cloud/aws: fixed max attempts retry issue; metric: using noop logger for cw metric writer until proper fix is in place;

--- pkg/cloud/aws/awsv2_retry.go
+++ pkg/cloud/aws/awsv2_retry.go
@@ -1,11 +1,17 @@
 package aws
 
-import "github.com/aws/aws-sdk-go-v2/aws/retry"
+import (
+	"math"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws/retry"
+)
 
 func DefaultClientRetryOptions(clientConfig ClientConfigAware) []func(*retry.StandardOptions) {
 	settings := clientConfig.GetSettings()
 
 	options := []func(*retry.StandardOptions){
+		RetryWithMaxBackoff(settings.Backoff.MaxInterval),
 		RetryWithMaxAttempts(settings.Backoff.MaxAttempts),
 		RetryWithBackoff(NewBackoffDelayer(settings.Backoff.InitialInterval, settings.Backoff.MaxInterval)),
 		RetryWithRateLimiter(NewNopRateLimiter()),
@@ -18,7 +24,17 @@ func DefaultClientRetryOptions(clientConfig ClientConfigAware) []func(*retry.Sta
 
 func RetryWithMaxAttempts(maxAttempts int) func(*retry.StandardOptions) {
 	return func(options *retry.StandardOptions) {
+		if maxAttempts <= 0 {
+			maxAttempts = math.MaxInt
+		}
+
 		options.MaxAttempts = maxAttempts
+	}
+}
+
+func RetryWithMaxBackoff(maxBackoff time.Duration) func(*retry.StandardOptions) {
+	return func(options *retry.StandardOptions) {
+		options.MaxBackoff = maxBackoff
 	}
 }
 
