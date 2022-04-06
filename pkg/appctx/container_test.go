1413195c79473012e697c2728d6c80a65265f2dd
Liviu-Adrian Esanu <liviu@justdice.io>
2022-04-06 08:10:27 +0200

    appctx/container.go now uses generics;

--- pkg/appctx/container_test.go
+++ pkg/appctx/container_test.go
@@ -2,25 +2,27 @@ package appctx_test
 
 import (
 	"context"
-	"testing"
-
 	"github.com/justtrackio/gosoline/pkg/appctx"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestProvide(t *testing.T) {
+	type customType struct {
+		val string
+	}
 	ctx := appctx.WithContainer(context.Background())
 
-	val, err := appctx.Provide(ctx, "foo", func() (interface{}, error) {
-		return "bar", nil
+	val, err := appctx.Provide[*customType](ctx, "foo", func() (*customType, error) {
+		return &customType{"bar"}, nil
 	})
 	assert.NoError(t, err)
-	assert.Equal(t, "bar", val)
+	assert.Equal(t, customType{"bar"}, *val)
 
-	val, err = appctx.Provide(ctx, "foo", func() (interface{}, error) {
+	val, err = appctx.Provide[*customType](ctx, "foo", func() (*customType, error) {
 		assert.FailNow(t, "the factory should not be called a second time")
-		return "bar", nil
+		return &customType{}, nil
 	})
 	assert.NoError(t, err)
-	assert.Equal(t, "bar", val)
+	assert.Equal(t, customType{"bar"}, *val)
 }
