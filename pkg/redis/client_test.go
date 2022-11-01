00f7920272fb6f54f04363bc71ebb3333363273e
Basti Funck <basti.funck@gmail.com>
2022-11-01 17:25:20 +0100

    redis: add sorted sets operations

--- pkg/redis/client_test.go
+++ pkg/redis/client_test.go
@@ -240,6 +240,134 @@ func (s *ClientWithMiniRedisTestSuite) TestDecr() {
 	s.Equal(int64(3), val)
 }
 
+func (s *ClientWithMiniRedisTestSuite) TestZAdd() {
+	inserts, err := s.client.ZAdd(context.Background(), "key", 10.0, "member")
+	s.NoError(err, "there should be no error on ZAdd")
+	s.EqualValues(1, inserts)
+}
+
+func (s *ClientWithMiniRedisTestSuite) TestZCard() {
+	cardinality, err := s.client.ZCard(context.Background(), "key")
+	s.NoError(err, "there should be no error on ZCard")
+	s.EqualValues(0, cardinality)
+
+	_, err = s.client.ZAdd(context.Background(), "key", 10.0, "member0")
+	s.NoError(err, "there should be no error on ZAdd")
+
+	_, err = s.client.ZAdd(context.Background(), "key", 10.0, "member1")
+	s.NoError(err, "there should be no error on ZAdd")
+
+	_, err = s.client.ZAdd(context.Background(), "key", 11.0, "member2")
+	s.NoError(err, "there should be no error on ZAdd")
+
+	_, err = s.client.ZAdd(context.Background(), "key", 12.0, "member3")
+	s.NoError(err, "there should be no error on ZAdd")
+
+	cardinality, err = s.client.ZCard(context.Background(), "key")
+	s.NoError(err, "there should be no error on ZCard")
+	s.EqualValues(4, cardinality)
+}
+
+func (s *ClientWithMiniRedisTestSuite) TestZScore() {
+	_, err := s.client.ZAdd(context.Background(), "key", 10.0, "member")
+
+	score, err := s.client.ZScore(context.Background(), "key", "member")
+	s.NoError(err, "there should be no error on ZScore")
+	s.EqualValues(10.0, score)
+}
+
+func (s *ClientWithMiniRedisTestSuite) TestZRange() {
+	_, err := s.client.ZAdd(context.Background(), "key", 10, "member0")
+	_, err = s.client.ZAdd(context.Background(), "key", 50, "member1")
+	_, err = s.client.ZAdd(context.Background(), "key", 90, "member2")
+	_, err = s.client.ZAdd(context.Background(), "key", 100, "member3")
+	_, err = s.client.ZAdd(context.Background(), "key", 120, "member4")
+
+	members, err := s.client.ZRange(context.Background(), "key", 1, 2)
+	s.NoError(err, "there should be no error on ZRange")
+	s.EqualValues(2, len(members))
+	s.Equal("member1", members[0])
+	s.Equal("member2", members[1])
+}
+
+func (s *ClientWithMiniRedisTestSuite) TestZRevRange() {
+	_, err := s.client.ZAdd(context.Background(), "key", 10, "member0")
+	_, err = s.client.ZAdd(context.Background(), "key", 50, "member1")
+	_, err = s.client.ZAdd(context.Background(), "key", 90, "member2")
+	_, err = s.client.ZAdd(context.Background(), "key", 100, "member3")
+	_, err = s.client.ZAdd(context.Background(), "key", 120, "member4")
+
+	members, err := s.client.ZRevRange(context.Background(), "key", 1, 2)
+	s.NoError(err, "there should be no error on ZRevRange")
+	s.EqualValues(2, len(members))
+	s.Equal("member3", members[0])
+	s.Equal("member2", members[1])
+}
+
+func (s *ClientWithMiniRedisTestSuite) TestZRank() {
+	_, err := s.client.ZAdd(context.Background(), "key", 10, "member0")
+	_, err = s.client.ZAdd(context.Background(), "key", 50, "member1")
+	_, err = s.client.ZAdd(context.Background(), "key", 90, "member2")
+	_, err = s.client.ZAdd(context.Background(), "key", 100, "member3")
+	_, err = s.client.ZAdd(context.Background(), "key", 120, "member4")
+
+	rank, err := s.client.ZRank(context.Background(), "key", "member3")
+	s.NoError(err, "there should be no error on ZRank")
+	s.EqualValues(3, rank)
+}
+
+func (s *ClientWithMiniRedisTestSuite) TestZRevRank() {
+	_, err := s.client.ZAdd(context.Background(), "key", 10, "member0")
+	_, err = s.client.ZAdd(context.Background(), "key", 50, "member1")
+	_, err = s.client.ZAdd(context.Background(), "key", 90, "member2")
+	_, err = s.client.ZAdd(context.Background(), "key", 100, "member3")
+	_, err = s.client.ZAdd(context.Background(), "key", 120, "member4")
+
+	rank, err := s.client.ZRevRank(context.Background(), "key", "member3")
+	s.NoError(err, "there should be no error on ZRevRank")
+	s.EqualValues(1, rank)
+}
+
+func (s *ClientWithMiniRedisTestSuite) TestZRem() {
+	_, err := s.client.ZAdd(context.Background(), "key", 10, "member0")
+	_, err = s.client.ZAdd(context.Background(), "key", 50, "member1")
+	_, err = s.client.ZAdd(context.Background(), "key", 90, "member2")
+	_, err = s.client.ZAdd(context.Background(), "key", 100, "member3")
+	_, err = s.client.ZAdd(context.Background(), "key", 120, "member4")
+
+	removes, err := s.client.ZRem(context.Background(), "key", "member2", "member3", "memberX")
+	s.NoError(err, "there should be no error on ZRem")
+	s.EqualValues(2, removes)
+}
+
+// Following commented redis v8 operations are not supported by miniredis yet
+// so we can't unit-test them.
+// Issue: https://github.com/alicebob/miniredis/issues/302
+// TODO: Check if miniredis supports redis v8 already and update
+
+//func (s *ClientWithMiniRedisTestSuite) TestZRandMember() {
+//
+//	_, err := s.client.ZAdd(context.Background(), "key", 10, "member0")
+//	_, err = s.client.ZAdd(context.Background(), "key", 50, "member1")
+//	_, err = s.client.ZAdd(context.Background(), "key", 90, "member2")
+//	_, err = s.client.ZAdd(context.Background(), "key", 100, "member3")
+//	_, err = s.client.ZAdd(context.Background(), "key", 120, "member4")
+//
+//	members, err := s.client.ZRandMember(context.Background(), "key", 2, true)
+//	s.NoError(err, "there should be no error on ZRandMember")
+//	s.EqualValues(2, len(members))
+//}
+
+//func (s *ClientWithMiniRedisTestSuite) TestZMScore() {
+//	_, err := s.client.ZAdd(context.Background(), "key", 10, "member0")
+//	_, err = s.client.ZAdd(context.Background(), "key", 50, "member1")
+//
+//	scores, err := s.client.ZMScore(context.Background(), "key", "member0", "member1")
+//	s.NoError(err, "there should be no error on ZMScore")
+//	s.EqualValues(10.0, scores[0])
+//	s.EqualValues(50.0, scores[1])
+//}
+
 func (s *ClientWithMiniRedisTestSuite) TestExpire() {
 	_, _ = s.client.Incr(context.Background(), "key")
 
