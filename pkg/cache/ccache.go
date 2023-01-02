48526709929087e352d98744d047e84412b17f86
Anselm Jonas Scholl <aj@justtrack.io>
2023-01-02 16:07:23 +0100

    cache: add type parameter and made cache generic - breaking change;
    This change refactors the cache package and forces you to specify a type
    for the value you store in the cache (and frees you from manually
    casting the values returned from the cache to the correct type again).
    To improve the package further, two new methods were added which
    abstract over the check - request - set pattern. As a last change, you
    can now specify an option to also cache zero values (e.g., nil) for a
    specified time, allowing you to basically cache missing values as well.

--- pkg/cache/ccache.go
+++ pkg/cache/ccache.go
@@ -1,88 +1,160 @@
 package cache
 
 import (
+	"reflect"
 	"time"
 
 	"github.com/karlseguin/ccache"
 )
 
 //go:generate mockery --name Cache
-type Cache interface {
+type Cache[T any] interface {
 	Contains(key string) bool
 	Expire(key string) bool
-	Get(key string) (interface{}, bool)
-	Set(key string, value interface{})
-	SetX(key string, value interface{}, ttl time.Duration)
+	Get(key string) (T, bool)
+	Set(key string, value T)
+	SetX(key string, value T, ttl time.Duration)
+	Provide(key string, provider func() T) T
+	ProvideWithError(key string, provider func() (T, error)) (T, error)
 }
 
-type cache struct {
-	base *ccache.Cache
-	size int
-	ttl  time.Duration
+type cache[T any] struct {
+	base        *ccache.Cache
+	ttl         time.Duration
+	notFoundTtl time.Duration
 }
 
-func New(maxSize int64, pruneCount uint32, ttl time.Duration) Cache {
-	cache := &cache{
-		size: 0,
-		ttl:  ttl,
-	}
+type Option[T any] func(*cache[T])
 
+func New[T any](maxSize int64, pruneCount uint32, ttl time.Duration, options ...Option[T]) Cache[T] {
 	config := ccache.Configure()
 	config.MaxSize(maxSize)
 	config.ItemsToPrune(pruneCount)
 
-	cache.base = ccache.New(config)
-
-	return cache
+	return NewWithConfiguration(*config, ttl, options...)
 }
 
-func NewWithConfiguration(config ccache.Configuration, ttl time.Duration) Cache {
-	cache := &cache{
-		size: 0,
-		ttl:  ttl,
+func NewWithConfiguration[T any](config ccache.Configuration, ttl time.Duration, options ...Option[T]) Cache[T] {
+	cache := &cache[T]{
+		base:        ccache.New(&config),
+		ttl:         ttl,
+		notFoundTtl: 0,
 	}
-	cache.base = ccache.New(&config)
+
+	for _, opt := range options {
+		opt(cache)
+	}
 
 	return cache
 }
 
-func (c *cache) Set(key string, value interface{}) {
+func WithNotFoundTtl[T any](notFoundTtl time.Duration) func(cache *cache[T]) {
+	return func(cache *cache[T]) {
+		cache.notFoundTtl = notFoundTtl
+	}
+}
+
+func (c *cache[T]) Set(key string, value T) {
 	c.base.Set(key, value, c.ttl)
 }
 
-func (c *cache) SetX(key string, value interface{}, ttl time.Duration) {
+func (c *cache[T]) SetX(key string, value T, ttl time.Duration) {
 	c.base.Set(key, value, ttl)
 }
 
-func (c *cache) Get(key string) (interface{}, bool) {
+func (c *cache[T]) Get(key string) (T, bool) {
 	item := c.base.Get(key)
 
 	if item == nil {
-		return nil, false
+		var noResult T
+
+		return noResult, false
 	}
 
 	if item.Expired() {
-		return nil, false
+		var noResult T
+
+		return noResult, false
 	}
 
-	return item.Value(), true
+	value := item.Value().(T)
+
+	return value, true
 }
 
-func (c *cache) Contains(key string) bool {
+func (c *cache[T]) Contains(key string) bool {
 	_, ok := c.Get(key)
 
 	return ok
 }
 
-func (c *cache) Expire(key string) bool {
+func (c *cache[T]) Expire(key string) bool {
 	item := c.base.Get(key)
 
 	if item == nil {
 		return false
 	}
 
-	remaining := time.Until(item.Expires())
-	item.Extend(-1 * remaining)
+	// extend the time until the item expires to the current time minus one second
+	item.Extend(-time.Second)
 
 	return true
+}
+
+func (c *cache[T]) Provide(key string, provider func() T) T {
+	result, _ := provide(c, key, func() (T, bool) {
+		return provider(), true
+	})
+
+	return result
+}
+
+func (c *cache[T]) ProvideWithError(key string, provider func() (T, error)) (T, error) {
+	var err error
+
+	result, ok := provide(c, key, func() (T, bool) {
+		var innerResult T
+		innerResult, err = provider()
+
+		return innerResult, err == nil
+	})
+
+	if !ok {
+		var noResult T
+
+		return noResult, err
+	}
+
+	return result, nil
+}
+
+func provide[T any](c *cache[T], key string, provider func() (T, bool)) (T, bool) {
+	if result, ok := c.Get(key); ok {
+		return result, true
+	}
+
+	result, ok := provider()
+	if !ok {
+		return result, false
+	}
+
+	if !isZero(result) {
+		c.Set(key, result)
+	} else if c.notFoundTtl > 0 {
+		// cache a typed nil-ptr. when we look up a value the next time, we will be able to cast it back to the correct type.
+		c.SetX(key, result, c.notFoundTtl)
+	}
+
+	return result, true
+}
+
+func isZero[T any](v T) bool {
+	// ideally, we would like to do
+	//
+	// var zero T
+	// return v == zero
+	//
+	// However, that requires comparable and slices and functions are not comparable, but they can be nil and we would
+	// like to check for that
+	return reflect.ValueOf(&v).Elem().IsZero()
 }
--- pkg/cache/ccache_test.go
+++ pkg/cache/ccache_test.go
@@ -1,49 +1,204 @@
-package cache
+package cache_test
 
 import (
-	"github.com/karlseguin/ccache"
-	"github.com/stretchr/testify/assert"
+	"fmt"
 	"testing"
 	"time"
+
+	"github.com/justtrackio/gosoline/pkg/cache"
+	"github.com/karlseguin/ccache"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNew(t *testing.T) {
 	assert.NotPanics(t, func() {
-		New(1, 1, 1)
+		cache.New[string](1, 1, 1)
 	})
 	assert.NotPanics(t, func() {
-		New(1, 0, 1)
+		cache.New[string](1, 0, 1)
 	})
 }
 
 func TestNewWithConfiguration(t *testing.T) {
 	assert.NotPanics(t, func() {
-		NewWithConfiguration(ccache.Configuration{}, 1)
+		cache.NewWithConfiguration[string](ccache.Configuration{}, 1)
 	})
 }
 
-func TestCCache_Set(t *testing.T) {
+func TestCCache_Set_Get(t *testing.T) {
 	assert.NotPanics(t, func() {
-		cache := New(1, 0, 1)
+		c := cache.New[string](1, 0, time.Hour)
 
-		cache.Set("key", "value")
+		c.Set("key", "value")
+		value, ok := c.Get("key")
+
+		assert.True(t, ok)
+		assert.Equal(t, "value", value)
+	})
+}
+
+func TestCCache_SetX_Get(t *testing.T) {
+	assert.NotPanics(t, func() {
+		c := cache.New[string](1, 0, 0)
+
+		c.SetX("key", "value", time.Hour)
+		value, ok := c.Get("key")
+
+		assert.True(t, ok)
+		assert.Equal(t, "value", value)
 	})
 }
 
 func TestCCache_Contains(t *testing.T) {
 	assert.NotPanics(t, func() {
-		cache := New(1, 0, 1*time.Second)
+		c := cache.New[string](1, 0, time.Hour)
 
-		cache.Set("key", "value")
-		assert.Equal(t, true, cache.Contains("key"))
+		c.Set("key", "value")
+		assert.Equal(t, true, c.Contains("key"))
 	})
 }
 
 func TestCCache_Expire(t *testing.T) {
 	assert.NotPanics(t, func() {
-		cache := New(1, 0, 0)
+		c := cache.New[string](1, 0, 0)
 
-		cache.Set("key", "value")
-		assert.Equal(t, false, cache.Contains("key"))
+		c.Set("key", "value")
+		assert.Equal(t, false, c.Contains("key"))
+	})
+}
+
+func TestCCache_Expire_Manually(t *testing.T) {
+	assert.NotPanics(t, func() {
+		c := cache.New[string](1, 0, time.Hour)
+
+		c.Set("key", "value")
+		c.Expire("key")
+		assert.Equal(t, false, c.Contains("key"))
+	})
+}
+
+func TestCCache_Expire_Manually_DoesntResurrect(t *testing.T) {
+	assert.NotPanics(t, func() {
+		c := cache.New[string](1, 0, time.Millisecond*100)
+
+		c.Set("key", "value")
+		// expire the item naturally - we used to have a bug where an expired item would get resurrected
+		// if it was already expired (because we set the timestamp to expire it to NOW - (EXPIRE_AT - NOW)).
+		// When (EXPIRE_AT - NOW) (i.e., the remaining time) is negative, the new timestamp was then in the future. Yikes!
+		time.Sleep(time.Millisecond * 300)
+		c.Expire("key")
+		assert.Equal(t, false, c.Contains("key"))
+	})
+}
+
+func TestCCache_Provide(t *testing.T) {
+	assert.NotPanics(t, func() {
+		c := cache.New[string](1, 0, time.Hour)
+
+		callCount := 0
+		value := c.Provide("key", func() string {
+			callCount++
+
+			return "value"
+		})
+
+		assert.Equal(t, 1, callCount)
+		assert.Equal(t, "value", value)
+
+		value = c.Provide("key", func() string {
+			callCount++
+
+			return "something else"
+		})
+
+		assert.Equal(t, 1, callCount)
+		assert.Equal(t, "value", value)
+	})
+}
+
+func TestCCache_Provide_NotFoundNotCached(t *testing.T) {
+	assert.NotPanics(t, func() {
+		c := cache.New[string](1, 0, time.Hour)
+
+		callCount := 0
+		value := c.Provide("key", func() string {
+			callCount++
+
+			return ""
+		})
+
+		assert.Equal(t, 1, callCount)
+		assert.Equal(t, "", value)
+
+		value = c.Provide("key", func() string {
+			callCount++
+
+			return "something"
+		})
+
+		assert.Equal(t, 2, callCount)
+		assert.Equal(t, "something", value)
+	})
+}
+
+func TestCCache_Provide_NotFoundCached(t *testing.T) {
+	assert.NotPanics(t, func() {
+		c := cache.New[string](1, 0, time.Hour, cache.WithNotFoundTtl[string](time.Hour))
+
+		callCount := 0
+		value := c.Provide("key", func() string {
+			callCount++
+
+			return ""
+		})
+
+		assert.Equal(t, 1, callCount)
+		assert.Equal(t, "", value)
+
+		value = c.Provide("key", func() string {
+			callCount++
+
+			return "something"
+		})
+
+		assert.Equal(t, 1, callCount)
+		assert.Equal(t, "", value)
+	})
+}
+
+func TestCCache_ProvideWithError(t *testing.T) {
+	assert.NotPanics(t, func() {
+		c := cache.New[string](1, 0, time.Hour)
+
+		callCount := 0
+		value, err := c.ProvideWithError("key", func() (string, error) {
+			callCount++
+
+			return "ignored", fmt.Errorf("failed")
+		})
+
+		assert.Equal(t, 1, callCount)
+		assert.EqualError(t, err, "failed")
+		assert.Equal(t, "", value)
+
+		value, err = c.ProvideWithError("key", func() (string, error) {
+			callCount++
+
+			return "value", nil
+		})
+
+		assert.Equal(t, 2, callCount)
+		assert.NoError(t, err)
+		assert.Equal(t, "value", value)
+
+		value, err = c.ProvideWithError("key", func() (string, error) {
+			callCount++
+
+			return "something else", nil
+		})
+
+		assert.Equal(t, 2, callCount)
+		assert.NoError(t, err)
+		assert.Equal(t, "value", value)
 	})
 }
