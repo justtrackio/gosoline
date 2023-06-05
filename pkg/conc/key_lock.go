02a5364fc680c6bfb1e61893efaaf7042238e9ee
Anselm Jonas Scholl <aj@justtrack.io>
2023-06-05 14:50:57 +0200

    conc: fixed KeyLock to only release resources after the key is completely unused;
    There used to be a case where we would acquire a lock entry for the
    KeyLock and lock it. Now a second thread would also get the lock from
    the map and try lock it. It would wait for the first thread to be done.
    Now the first thread is done and unlocks the entry and removes it from
    the map! The second thread locks it, but now nobody knows that lock
    anymore and a third thread could get a new entry for the same key and
    lock it, causing two thread to be in the same criticial section! Yikes!

--- pkg/conc/key_lock.go
+++ pkg/conc/key_lock.go
@@ -1,39 +1,54 @@
 package conc
 
-import "sync"
+import (
+	"sync"
+)
 
 type UnlockFunc func()
 
 type KeyLock interface {
-	// Lock based on the given key, returns an UnlockFunc when called it unlocks the underlying key and also
+	// Lock based on the given key, returns an UnlockFunc when called it unlocks the underlying key and
 	// releases the resources.
 	Lock(key any) UnlockFunc
 }
 
 type keyLock struct {
-	lck     *sync.Mutex
-	mutexes sync.Map
+	lck     sync.Mutex
+	entries map[any]*entry
 }
 
-func NewKeyLock() *keyLock {
+type entry struct {
+	sync.Mutex
+	refCount int
+}
+
+func NewKeyLock() KeyLock {
 	return &keyLock{
-		lck: &sync.Mutex{},
+		lck:     sync.Mutex{},
+		entries: map[any]*entry{},
 	}
 }
 
 func (l *keyLock) Lock(key any) UnlockFunc {
 	l.lck.Lock()
-	lockItem, _ := l.mutexes.LoadOrStore(key, &sync.Mutex{})
+	lockEntry, ok := l.entries[key]
+	if !ok {
+		lockEntry = &entry{}
+		l.entries[key] = lockEntry
+	}
+	lockEntry.refCount++
 	l.lck.Unlock()
 
-	mu := lockItem.(*sync.Mutex)
-	mu.Lock()
+	lockEntry.Lock()
 
 	return func() {
 		l.lck.Lock()
 
-		mu.Unlock()
-		l.mutexes.Delete(key)
+		lockEntry.refCount--
+		lockEntry.Unlock()
+		if lockEntry.refCount == 0 {
+			delete(l.entries, key)
+		}
 
 		l.lck.Unlock()
 	}
