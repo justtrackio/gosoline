eb1098f28b8896d8f4ac6fb7b166c35076701b80
Alexander Ronquillo <alexanderronquillo@ip-192-168-30-156.eu-central-1.compute.internal>
2023-09-01 13:42:13 +0200

    Re-organize, use verbs, add descriptions

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -24,7 +24,7 @@ const sidebars = {
     {
       type: 'category',
       label: 'Tutorial',
-      items: ['getting-started/create-a-document'],
+      items: ['get-started/create-a-document'],
     },
   ],
    */
