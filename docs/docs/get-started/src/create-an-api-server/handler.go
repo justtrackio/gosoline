2e9a8f1f58ccd336a2f05d7a80e79028d921efec
Alexander Ronquillo <alexanderronquillo@applikeITs-MacBook-Pro.local>
2023-09-05 17:05:28 +0200

    edit-quickstart

--- docs/docs/get-started/src/create-an-api-server/handler.go
+++ docs/docs/get-started/src/create-an-api-server/handler.go
@@ -9,19 +9,16 @@ import (
 	"github.com/justtrackio/gosoline/pkg/log"
 )
 
-// the struct used to bind the input: ?id=5&text=todo
 type Todo struct {
-	Id        int    `form:"id"` // form tag defines the query param name
+	Id        int    `form:"id"`
 	Text      string `form:"text"`
 	CreatedAt time.Time
 }
 
-// the actual todohandler struct
 type TodoHandler struct {
 	logger log.Logger
 }
 
-// returns the input instance to use
 func (t TodoHandler) GetInput() interface{} {
 	return &Todo{}
 }
@@ -34,19 +31,14 @@ func NewTodoHandler(ctx context.Context, config cfg.Config, logger log.Logger) (
 	return handler, nil
 }
 
-// this one is called for every request
 func (t TodoHandler) Handle(
-	// context of this request
 	ctx context.Context,
-	// request information (body, params, headers, cookies, client ip, ...)
 	request *apiserver.Request,
 ) (*apiserver.Response, error) {
-	// get the input data from request body
 	todo := request.Body.(*Todo)
 	todo.CreatedAt = time.Now()
 
 	t.logger.Info("got todo with id %d", todo.Id)
 
-	// write a json response with the todo
 	return apiserver.NewJsonResponse(todo), nil
 }
--- docs/docs/get-started/src/create-an-api-server/main.go
+++ docs/docs/get-started/src/create-an-api-server/main.go
@@ -11,24 +11,20 @@ import (
 )
 
 func main() {
-	// api server factory which defines the http routes
 	definer := func(ctx context.Context, config cfg.Config, logger log.Logger) (*apiserver.Definitions, error) {
 		def := &apiserver.Definitions{}
 
 		var err error
 		var handler apiserver.HandlerWithInput
 
-		// create a handler which handles input data
 		if handler, err = NewTodoHandler(ctx, config, logger); err != nil {
 			return nil, fmt.Errorf("can not create trip handler: %w", err)
 		}
 
-		// bind this handler with input form the query params to the todo path with GET method
 		def.GET("/todo", apiserver.CreateQueryHandler(handler))
 
 		return def, nil
 	}
 
-	// runs an api server application based on the definitions
 	application.RunApiServer(definer)
 }
