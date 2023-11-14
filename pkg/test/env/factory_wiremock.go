6f2dd8bc103db221c975101d4a7346dabcde0183
Jan Kamieth <jan@justtrack.io>
2023-11-14 15:36:22 +0100

    test: update wiremock image to 3.3.1-alpine;

--- pkg/test/env/factory_wiremock.go
+++ pkg/test/env/factory_wiremock.go
@@ -48,8 +48,7 @@ func (f *wiremockFactory) configureContainer(settings interface{}) *containerCon
 
 	return &containerConfig{
 		Repository: "wiremock/wiremock",
-		// alpine version doesn't run on arm based chips that support x86/x64 emulation, main does have an arm version but is not a specific version
-		Tag: "3.2.0-2",
+		Tag:        "3.3.1-alpine",
 		PortBindings: portBindings{
 			"8080/tcp": s.Port,
 		},
