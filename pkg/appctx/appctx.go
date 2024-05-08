33e64c91900a82aad5c824f25d091b39590ef0e1
Benedikt T <168745819+bt-justtrack@users.noreply.github.com>
2024-05-08 09:37:46 +0200

    appctx: Add documentation

---
+++ pkg/appctx/appctx.go
@@ -0,0 +1,5 @@
+// Package appctx provides a [context.Context] based mechanism of sharing certain "global" resources
+// across different parts of an application.
+// When building applications with the [github.com/justtrackio/gosoline/pkg/application] package
+// a Container is automatically injected into the applications context.
+package appctx
