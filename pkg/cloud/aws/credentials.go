85b0032248f85ad40571f52826b0810d69f2df8f
Anselm Jonas Scholl <aj@justtrack.io>
2024-06-13 11:09:46 +0200

    cloud/aws: added defaults for assume_role to the cloud.aws.defaults key;

--- pkg/cloud/aws/credentials.go
+++ pkg/cloud/aws/credentials.go
@@ -9,10 +9,9 @@ import (
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
-	"github.com/justtrackio/gosoline/pkg/cfg"
 )
 
-func GetCredentialsProvider(ctx context.Context, config cfg.Config, settings ClientSettings) (aws.CredentialsProvider, error) {
+func GetCredentialsProvider(ctx context.Context, settings ClientSettings) (aws.CredentialsProvider, error) {
 	if settings.Credentials.AccessKeyID != "" {
 		return credentials.NewStaticCredentialsProvider(settings.Credentials.AccessKeyID, settings.Credentials.SecretAccessKey, settings.Credentials.SessionToken), nil
 	}
@@ -21,33 +20,18 @@ func GetCredentialsProvider(ctx context.Context, config cfg.Config, settings Cli
 		return GetAssumeRoleCredentialsProvider(ctx, settings.AssumeRole)
 	}
 
-	if creds := UnmarshalDefaultCredentials(config); creds != nil {
-		return credentials.NewStaticCredentialsProvider(creds.AccessKeyID, creds.SecretAccessKey, creds.SessionToken), nil
-	}
-
 	return GetDefaultProvider(), nil
 }
 
 func GetAssumeRoleCredentialsProvider(ctx context.Context, roleArn string) (aws.CredentialsProvider, error) {
 	var err error
-	var cfg aws.Config
+	var conf aws.Config
 
-	if cfg, err = awsCfg.LoadDefaultConfig(ctx); err != nil {
+	if conf, err = awsCfg.LoadDefaultConfig(ctx); err != nil {
 		return nil, fmt.Errorf("can not load default aws config: %w", err)
 	}
 
-	stsClient := sts.NewFromConfig(cfg)
+	stsClient := sts.NewFromConfig(conf)
 
 	return stscreds.NewAssumeRoleProvider(stsClient, roleArn), nil
-}
-
-func UnmarshalDefaultCredentials(config cfg.Config) *Credentials {
-	if !config.HasPrefix("cloud.aws.credentials") {
-		return nil
-	}
-
-	creds := &Credentials{}
-	config.UnmarshalKey("cloud.aws.credentials", creds)
-
-	return creds
 }
