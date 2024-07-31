5294cfa5760346d37f1e8dcdadf8cdfe708ba116
Benedikt T <168745819+bt-justtrack@users.noreply.github.com>
2024-07-31 16:02:53 +0200

    allow restricting available auth options through config

--- pkg/httpserver/auth/auth.go
+++ pkg/httpserver/auth/auth.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/justtrackio/gosoline/pkg/cfg"
+	"github.com/justtrackio/gosoline/pkg/funk"
 )
 
 const Anonymous = "anon"
@@ -38,4 +40,15 @@ func GetSubject(ctx context.Context) *Subject {
 	}
 
 	panic(fmt.Errorf("there is no subject in the context"))
+}
+
+func OnlyConfiguredAuthenticators(config cfg.Config, name string, authenticators map[string]Authenticator) map[string]Authenticator {
+	key := fmt.Sprintf("httpserver.%s.auth", name)
+	settings := &Settings{}
+	config.UnmarshalKey(key, settings)
+	if len(settings.AllowedAuthenticators) == 0 {
+		return authenticators
+	}
+
+	return funk.IntersectMaps(authenticators, funk.SliceToMap(settings.AllowedAuthenticators, func(method string) (string, Authenticator) { return method, nil }))
 }
