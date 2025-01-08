13926a095f90102e1216d319426dac6612637606
Anselm Jonas Scholl <aj@justtrack.io>
2025-01-08 15:34:48 +0100

    stream: avoid race condition when ingesting data;

--- pkg/stream/consumer_base.go
+++ pkg/stream/consumer_base.go
@@ -261,8 +261,12 @@ func (c *baseConsumer) ingestData(ctx context.Context) error {
 	defer close(c.data)
 
 	cfn := coffin.New()
-	cfn.GoWithContextf(ctx, c.ingestDataFromSource(c.input, dataSourceInput), "panic during shoveling data from input")
-	cfn.GoWithContextf(ctx, c.ingestDataFromSource(c.retryInput, dataSourceRetry), "panic during shoveling data from retry")
+	cfn.Go(func() error {
+		cfn.GoWithContextf(ctx, c.ingestDataFromSource(c.input, dataSourceInput), "panic during shoveling data from input")
+		cfn.GoWithContextf(ctx, c.ingestDataFromSource(c.retryInput, dataSourceRetry), "panic during shoveling data from retry")
+
+		return nil
+	})
 
 	return cfn.Wait()
 }
