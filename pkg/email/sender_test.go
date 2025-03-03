f471c2cc8e422fce2030c6cf5be941ea21e8f680
David Hand <davidhand1988@gmail.com>
2025-03-03 17:13:54 +0100

    add support for AWS SES email sending

---
+++ pkg/email/sender_test.go
@@ -0,0 +1,155 @@
+package email_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sesv2"
+	"github.com/aws/aws-sdk-go-v2/service/sesv2/types"
+	"github.com/justtrackio/gosoline/pkg/cloud/aws/ses/mocks"
+	"github.com/justtrackio/gosoline/pkg/email"
+	loggerMocks "github.com/justtrackio/gosoline/pkg/log/mocks"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
+
+type SenderTestSuite struct {
+	suite.Suite
+
+	sender email.Sender
+
+	logger *loggerMocks.Logger
+	client *mocks.Client
+	ctx    context.Context
+}
+
+func TestSenderTestSuite(t *testing.T) {
+	suite.Run(t, new(SenderTestSuite))
+}
+
+func (s *SenderTestSuite) SetupTest() {
+	s.logger = new(loggerMocks.Logger)
+	s.client = mocks.NewClient(s.T())
+
+	s.sender = email.NewSenderWithInterfaces(
+		s.logger,
+		s.client,
+		"sender@example.com",
+	)
+
+	s.ctx = context.Background()
+}
+
+func (s *SenderTestSuite) TearDownTest() {
+	s.client.AssertExpectations(s.T())
+}
+
+func (s *SenderTestSuite) TestSendEmail_TextEmail() {
+	recipients := []string{"recipient@example.com"}
+	subject := "Test Subject"
+	body := "This is a plain text email."
+
+	expectedEmailInput := &sesv2.SendEmailInput{
+		FromEmailAddress: aws.String("sender@example.com"),
+		Destination: &types.Destination{
+			ToAddresses: recipients,
+		},
+		Content: &types.EmailContent{
+			Simple: &types.Message{
+				Subject: &types.Content{Data: aws.String(subject)},
+				Body: &types.Body{
+					Text: &types.Content{Data: aws.String(body)},
+					Html: &types.Content{Data: nil},
+				},
+			},
+		},
+	}
+
+	s.client.EXPECT().SendEmail(mock.Anything, expectedEmailInput).Return(&sesv2.SendEmailOutput{}, nil)
+
+	err := s.sender.SendEmail(s.ctx, recipients, subject, &body, nil)
+	s.NoError(err)
+}
+
+func (s *SenderTestSuite) TestSendEmail_HtmlEmail() {
+	recipients := []string{"recipient@example.com"}
+	subject := "Test Subject"
+	htmlBody := "<h1>This is an HTML email.</h1>"
+
+	expectedEmailInput := &sesv2.SendEmailInput{
+		FromEmailAddress: aws.String("sender@example.com"),
+		Destination: &types.Destination{
+			ToAddresses: recipients,
+		},
+		Content: &types.EmailContent{
+			Simple: &types.Message{
+				Subject: &types.Content{Data: aws.String(subject)},
+				Body: &types.Body{
+					Text: &types.Content{Data: nil},
+					Html: &types.Content{Data: aws.String(htmlBody)},
+				},
+			},
+		},
+	}
+
+	s.client.EXPECT().SendEmail(mock.Anything, expectedEmailInput).Return(&sesv2.SendEmailOutput{}, nil)
+
+	err := s.sender.SendEmail(s.ctx, recipients, subject, nil, &htmlBody)
+	s.NoError(err)
+}
+
+func (s *SenderTestSuite) TestSendEmail_MultiFormatEmail() {
+	recipients := []string{"recipient@example.com"}
+	subject := "Test Subject"
+	body := "This is a plain text email."
+	htmlBody := "<h1>This is an HTML email.</h1>"
+
+	expectedEmailInput := &sesv2.SendEmailInput{
+		FromEmailAddress: aws.String("sender@example.com"),
+		Destination: &types.Destination{
+			ToAddresses: recipients,
+		},
+		Content: &types.EmailContent{
+			Simple: &types.Message{
+				Subject: &types.Content{Data: aws.String(subject)},
+				Body: &types.Body{
+					Text: &types.Content{Data: aws.String(body)},
+					Html: &types.Content{Data: aws.String(htmlBody)},
+				},
+			},
+		},
+	}
+
+	s.client.EXPECT().SendEmail(mock.Anything, expectedEmailInput).Return(&sesv2.SendEmailOutput{}, nil)
+
+	err := s.sender.SendEmail(s.ctx, recipients, subject, &body, &htmlBody)
+	s.NoError(err)
+}
+
+func (s *SenderTestSuite) TestSendEmail_ErrorFromSES() {
+	recipients := []string{"recipient@example.com"}
+	subject := "Test Error Handling"
+	body := "This email should trigger an error."
+
+	expectedEmailInput := &sesv2.SendEmailInput{
+		FromEmailAddress: aws.String("sender@example.com"),
+		Destination: &types.Destination{
+			ToAddresses: recipients,
+		},
+		Content: &types.EmailContent{
+			Simple: &types.Message{
+				Subject: &types.Content{Data: aws.String(subject)},
+				Body: &types.Body{
+					Text: &types.Content{Data: aws.String(body)},
+					Html: &types.Content{Data: nil},
+				},
+			},
+		},
+	}
+	s.client.EXPECT().SendEmail(mock.Anything, expectedEmailInput).Return(nil, errors.New("error"))
+
+	err := s.sender.SendEmail(s.ctx, recipients, subject, &body, nil)
+	s.Error(err)
+}
