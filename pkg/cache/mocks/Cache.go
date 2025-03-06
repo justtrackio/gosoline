dfb08ac5eae054bbbecb6509862a0901179877ce
Anselm Jonas Scholl <aj@justtrack.io>
2025-03-06 14:45:11 +0100

    cache: added Stop() method to shut down internal worker when no longer needed;
    The cache we are using has an internal go routine which removes old data
    from the cache as needed. We now expose the method to allow tests or
    other code to stop this worker when no longer needed.

--- pkg/cache/mocks/Cache.go
+++ pkg/cache/mocks/Cache.go
@@ -483,6 +483,38 @@ func (_c *Cache_SetX_Call[T]) RunAndReturn(run func(string, T, time.Duration)) *
 	return _c
 }
 
+// Stop provides a mock function with given fields:
+func (_m *Cache[T]) Stop() {
+	_m.Called()
+}
+
+// Cache_Stop_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'Stop'
+type Cache_Stop_Call[T interface{}] struct {
+	*mock.Call
+}
+
+// Stop is a helper method to define mock.On call
+func (_e *Cache_Expecter[T]) Stop() *Cache_Stop_Call[T] {
+	return &Cache_Stop_Call[T]{Call: _e.mock.On("Stop")}
+}
+
+func (_c *Cache_Stop_Call[T]) Run(run func()) *Cache_Stop_Call[T] {
+	_c.Call.Run(func(args mock.Arguments) {
+		run()
+	})
+	return _c
+}
+
+func (_c *Cache_Stop_Call[T]) Return() *Cache_Stop_Call[T] {
+	_c.Call.Return()
+	return _c
+}
+
+func (_c *Cache_Stop_Call[T]) RunAndReturn(run func()) *Cache_Stop_Call[T] {
+	_c.Call.Return(run)
+	return _c
+}
+
 // NewCache creates a new instance of Cache. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
 // The first argument is typically a *testing.T value.
 func NewCache[T interface{}](t interface {
