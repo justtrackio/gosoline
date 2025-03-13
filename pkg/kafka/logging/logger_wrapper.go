4d9f2bc6c55faaaf4d0001a3a181898e4450a48e
Erdal Acar <erdal@justtrack.io>
2025-03-13 14:38:19 +0100

    kafka: log non-critical errors as info

--- pkg/kafka/logging/logger_wrapper.go
+++ pkg/kafka/logging/logger_wrapper.go
@@ -1,7 +1,40 @@
 package logging
 
-type LoggerWrapper func(template string, values ...interface{})
+import (
+	"strings"
 
-func (log LoggerWrapper) Printf(t string, v ...interface{}) {
-	log(t, v...)
+	"github.com/justtrackio/gosoline/pkg/funk"
+)
+
+var nonCriticalErrors = []string{
+	// the clients metadata might be outdated if the partition leader has meanwhile changed.
+	// the kafka library will log an error in this case before retrying with the proper leader but
+	// this is not critical unless the max retry attempts are exceeded and in that case
+	// the library will actually return the error instead of logging it via the provided logger.
+	"Not Leader For Partition",
+	"not the leader",
+}
+
+func isNonCriticalError(msg string) bool {
+	return funk.ContainsFunc(nonCriticalErrors, func(nonCriticalError string) bool {
+		return strings.Contains(msg, nonCriticalError)
+	})
+}
+
+type DebugLoggerWrapper KafkaLogger
+
+func (logger DebugLoggerWrapper) Printf(msg string, args ...any) {
+	logger.Debug(msg, args...)
+}
+
+type ErrorLoggerWrapper KafkaLogger
+
+func (logger ErrorLoggerWrapper) Printf(msg string, args ...any) {
+	if isNonCriticalError(msg) {
+		logger.Info(msg, args...)
+
+		return
+	}
+
+	logger.Error(msg, args...)
 }
