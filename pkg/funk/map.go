f6712513a84653cf13850f0195fa52b89bf3d003
Anselm Jonas Scholl <aj@justtrack.io>
2025-03-18 16:46:45 +0100

    funk, mdl: added some more convenience functions (and tests);
    - funk.MergeMapsWith: Combine maps, but select elements using a function
      instead of using the one from the last map
    - funk.IntersectMapsWith: Intersect maps, but use a combination function
      again
    - funk.MapKeysWith: When mapping two keys to the same one, use a
      combination function to produce the final value
    - funk.PopulateMap, funk.PopulateMapWith: Create a map from a list of
      keys and a single value or a function generating values from the keys
    - mdl.IsEmpty: Simple check if something is the zero value
    - mdl.FirstNonEmpty: Convenience function to select the first provided
      value which is not empty

--- pkg/funk/map.go
+++ pkg/funk/map.go
@@ -1,5 +1,8 @@
 package funk
 
+// MergeMaps merges zero or more maps into one combined map. Elements from later arguments overwrite elements
+// from earlier arguments in the case of collisions. If given a single argument, MergeMaps produces a swallow
+// copy of the input.
 func MergeMaps[K comparable, V any, M ~map[K]V](m ...M) M {
 	var length int
 	for _, item := range m {
@@ -16,11 +19,39 @@ func MergeMaps[K comparable, V any, M ~map[K]V](m ...M) M {
 	return out
 }
 
-func IntersectMaps[K comparable, V any, M ~map[K]V](m1, m2 M) M {
+// MergeMapsWith is similar to MergeMaps, but uses a function to combine elements from keys present in multiple
+// input maps.
+func MergeMapsWith[K comparable, V any, M ~map[K]V](combine func(V, V) V, m ...M) M {
+	var length int
+	for _, item := range m {
+		length += len(item)
+	}
+
+	out := make(M, length)
+	for _, item := range m {
+		for k, v := range item {
+			if existing, ok := out[k]; ok {
+				out[k] = combine(existing, v)
+			} else {
+				out[k] = v
+			}
+		}
+	}
+
+	return out
+}
+
+// IntersectMaps produces the intersection of at least two maps. The resulting map contains all elements from
+// the first map with keys present in all input maps.
+func IntersectMaps[K comparable, V any, M ~map[K]V](m1, m2 M, ms ...M) M {
 	result := make(M)
 
 	for k, v := range m1 {
-		if _, ok := m2[k]; ok {
+		if _, ok := m2[k]; ok && All(ms, func(m3 M) bool {
+			_, ok := m3[k]
+
+			return ok
+		}) {
 			result[k] = v
 		}
 	}
@@ -28,6 +59,29 @@ func IntersectMaps[K comparable, V any, M ~map[K]V](m1, m2 M) M {
 	return result
 }
 
+// IntersectMapsWith is similar to IntersectMaps, but selects the values based on a combination function.
+func IntersectMapsWith[K comparable, V any, M ~map[K]V](combine func(V, V) V, m1, m2 M, ms ...M) M {
+	result := make(M)
+
+	for k, v1 := range m1 {
+		if v2, ok := m2[k]; ok && All(ms, func(m3 M) bool {
+			_, ok := m3[k]
+
+			return ok
+		}) {
+			result[k] = combine(v1, v2)
+			for _, m3 := range ms {
+				result[k] = combine(result[k], m3[k])
+			}
+		}
+	}
+
+	return result
+}
+
+// DifferenceMaps splits two maps into two new maps. The first result contains only the part of the left input
+// map not present in the right input map while the second result contains the part from the right input map
+// without keys in the left input map.
 func DifferenceMaps[K comparable, V1, V2 any, M1 ~map[K]V1, M2 ~map[K]V2](left M1, right M2) (inLeft M1, inRight M2) {
 	inLeft, inRight = make(M1, len(left)), make(M2, len(right))
 
@@ -46,6 +100,7 @@ func DifferenceMaps[K comparable, V1, V2 any, M1 ~map[K]V1, M2 ~map[K]V2](left M
 	return inLeft, inRight
 }
 
+// Keys returns the keys from a map as a slice.
 func Keys[K comparable, V any, M ~map[K]V](m M) []K {
 	keys := make([]K, 0, len(m))
 
@@ -56,6 +111,8 @@ func Keys[K comparable, V any, M ~map[K]V](m M) []K {
 	return keys
 }
 
+// MapKeys applies a function to every key from a map. If the function maps two keys to the same new value,
+// the result is undefined (one of the values will be randomly chosen).
 func MapKeys[K1 comparable, K2 comparable, V any, M1 ~map[K1]V](m M1, f func(key K1) K2) map[K2]V {
 	r := make(map[K2]V, len(m))
 
@@ -66,6 +123,25 @@ func MapKeys[K1 comparable, K2 comparable, V any, M1 ~map[K1]V](m M1, f func(key
 	return r
 }
 
+// MapKeysWith is similar to MapKeys, but uses a combination function to resolve conflicts when mapping two keys
+// to the same new key value. The order of the arguments of the combination function is randomly chosen and not
+// defined.
+func MapKeysWith[K1 comparable, K2 comparable, V any, M1 ~map[K1]V](m M1, f func(key K1) K2, combine func(V, V) V) map[K2]V {
+	r := make(map[K2]V, len(m))
+
+	for k, v := range m {
+		newKey := f(k)
+		if existing, ok := r[newKey]; ok {
+			r[newKey] = combine(existing, v)
+		} else {
+			r[newKey] = v
+		}
+	}
+
+	return r
+}
+
+// MapValues applies a function to all values of a map.
 func MapValues[K comparable, V1, V2 any, M1 ~map[K]V1](m M1, f func(value V1) V2) map[K]V2 {
 	r := make(map[K]V2, len(m))
 
@@ -74,4 +150,26 @@ func MapValues[K comparable, V1, V2 any, M1 ~map[K]V1](m M1, f func(value V1) V2
 	}
 
 	return r
+}
+
+// PopulateMap creates a new map from a value and a list of keys. All keys will be mapped to the given value.
+func PopulateMap[V any, K comparable](value V, keys ...K) map[K]V {
+	result := make(map[K]V, len(keys))
+
+	for _, key := range keys {
+		result[key] = value
+	}
+
+	return result
+}
+
+// PopulateMapWith creates a new map from a function and a list of keys by applying the function to each key.
+func PopulateMapWith[T any, K comparable](generator func(K) T, keys ...K) map[K]T {
+	result := make(map[K]T, len(keys))
+
+	for _, key := range keys {
+		result[key] = generator(key)
+	}
+
+	return result
 }
