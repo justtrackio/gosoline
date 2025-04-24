7e6c8425cf3aa81ffd01b8b7abd6d5ad18738355
Anselm Jonas Scholl <aj@justtrack.io>
2025-04-24 17:34:41 +0200

    funk: added more methods to the Set datatype;
    - `Set.Set` has been deprecated in favor of `Set.Add`
    - `NewSet` has been added as a convenience method
    - `Set.AddAll` has been added to merge a set into another set
    - `Set.Remove` and `Set.RemoveAll` have been added
    - `Set.ContainsAll` has been added to check inclusion of a whole set
    - `Set.Len` and `Set.Empty` have been added to query the size of a set
    
    The `Add` and `Remove` methods now accept multiple items and return if
    the set changed as the result of the operation.

--- pkg/funk/set.go
+++ pkg/funk/set.go
@@ -3,15 +3,112 @@ package funk
 // A Set is a map which stores no values.
 type Set[T comparable] map[T]struct{}
 
-// Set adds a new value to the set.
+// NewSet creates a new Set with the given elements.
+func NewSet[T comparable](items ...T) Set[T] {
+	s := make(Set[T], len(items))
+	s.Add(items...)
+
+	return s
+}
+
+// Set adds a new value to the Set.
+//
+// Deprecated: Use Add instead
 func (s Set[T]) Set(item T) {
 	s[item] = struct{}{}
 }
 
-// Contains checks if a set contains the given value.
+// Add adds new values to the Set.
+//
+// It returns true if any item was not contained in this Set.
+func (s Set[T]) Add(items ...T) bool {
+	result := false
+	for _, item := range items {
+		if _, ok := s[item]; ok {
+			continue
+		}
+
+		s[item] = struct{}{}
+		result = true
+	}
+
+	return result
+}
+
+// AddAll adds all elements of a Set to the Set.
+//
+// It returns true if any item was not contained in this Set.
+func (s Set[T]) AddAll(items Set[T]) bool {
+	result := false
+	for item := range items {
+		if s.Add(item) {
+			result = true
+		}
+	}
+
+	return result
+}
+
+// Remove removes values from the Set.
+//
+// It returns true if any item was contained in the Set.
+func (s Set[T]) Remove(items ...T) bool {
+	result := false
+	for _, item := range items {
+		if _, ok := s[item]; !ok {
+			continue
+		}
+
+		delete(s, item)
+		result = true
+	}
+
+	return result
+}
+
+// RemoveAll removes all elements of a Set from the Set.
+//
+// It returns true if any item was contained in the Set.
+func (s Set[T]) RemoveAll(items Set[T]) bool {
+	result := false
+	for item := range items {
+		if _, ok := s[item]; !ok {
+			continue
+		}
+
+		delete(s, item)
+		result = true
+	}
+
+	return result
+}
+
+// Contains checks if a Set contains the given value.
 func (s Set[T]) Contains(item T) bool {
 	_, ok := s[item]
+
 	return ok
+}
+
+// ContainsAll checks if a Set contains all values from the given other Set.
+func (s Set[T]) ContainsAll(other Set[T]) bool {
+	for item := range other {
+		if !s.Contains(item) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Len returns the number of distinct elements in the Set.
+func (s Set[T]) Len() int {
+	return len(s)
+}
+
+// Empty returns true if the Set contains no entries.
+func (s Set[T]) Empty() bool {
+	return len(s) == 0
 }
 
 // SetToSlice converts a set to a slice containing the elements from the set.
