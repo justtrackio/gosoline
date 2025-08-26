cab2f09d7efb6f272ec3ffaddc5bc1c907ffd141
Sebastian Struß <struss@justtrack.io>
2025-08-26 14:54:21 +0200

    test: add test for multiple blob stores running side-by-side

--- test/blob/store_test.go
+++ test/blob/store_test.go
@@ -3,10 +3,11 @@
 package blob_test
 
 import (
+	"bytes"
 	"fmt"
 	"testing"
+	"time"
 
-	awsS3 "github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/justtrackio/gosoline/pkg/blob"
 	"github.com/justtrackio/gosoline/pkg/clock"
 	"github.com/justtrackio/gosoline/pkg/cloud/aws/s3"
@@ -16,6 +17,9 @@ import (
 
 type StoreTestSuite struct {
 	suite.Suite
+
+	s3Client s3.Client
+	stores   map[string]blob.Store
 }
 
 func (s *StoreTestSuite) SetupSuite() []suite.Option {
@@ -24,27 +28,43 @@ func (s *StoreTestSuite) SetupSuite() []suite.Option {
 		suite.WithLogLevel("debug"),
 		suite.WithClockProvider(clock.NewRealClock()),
 		suite.WithModule("blob-store-runner-default", blob.ProvideBatchRunner("default")),
+		suite.WithModule("blob-store-runner-foo", blob.ProvideBatchRunner("foo")),
 	}
 }
 
+func (s *StoreTestSuite) SetupTest() error {
+	var err error
+
+	ctx := s.Env().Context()
+	config := s.Env().Config()
+	logger := s.Env().Logger()
+
+	s.stores = map[string]blob.Store{}
+	var stores map[string]any
+	if stores, err = config.GetStringMap("blob"); err != nil {
+		return err
+	}
+
+	for storeName := range stores {
+		if s.stores[storeName], err = blob.NewStore(ctx, config, logger, storeName); err != nil {
+			return err
+		}
+	}
+
+	if s.s3Client, err = s3.ProvideClient(ctx, config, logger, "default"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// This tests the following scenarios:
+// * general ability to store objects
+// * pagination of the ListObjects func of the store
 func (s *StoreTestSuite) TestNewDefault(_ suite.AppUnderTest) {
-	client, err := s3.ProvideClient(s.Env().Context(), s.Env().Config(), s.Env().Logger(), "default")
-	if !s.NoError(err) {
-		return
-	}
+	store := s.stores["default"]
 
-	_, err = client.CreateBucket(s.T().Context(), &awsS3.CreateBucketInput{
-		Bucket: mdl.Box("prj-test-fam"),
-	})
-	if !s.NoError(err) {
-		return
-	}
-
-	store, err := blob.NewStore(s.Env().Context(), s.Env().Config(), s.Env().Logger(), "default")
-	if !s.NoError(err) {
-		return
-	}
-
+	// make sure this exceeds the default batch size for ListObjectV2 requests, as we are testing the pagination here
 	size := 1001
 
 	batch := make(blob.Batch, size)
@@ -56,7 +76,7 @@ func (s *StoreTestSuite) TestNewDefault(_ suite.AppUnderTest) {
 		}
 	}
 
-	err = store.Write(batch)
+	err := store.Write(batch)
 	if !s.NoError(err) {
 		return
 	}
@@ -65,7 +85,54 @@ func (s *StoreTestSuite) TestNewDefault(_ suite.AppUnderTest) {
 	if !s.NoError(err) {
 		return
 	}
+
 	s.Len(newBatch, size)
+}
+
+// This Test is to ensure that we can write to multiple stores and that there are no stalled
+// go routines during that due to wrong handling in batch runner channel provider
+func (s *StoreTestSuite) TestMultiStoreNoBatchRunnerChannelIssues(_ suite.AppUnderTest) {
+	stores, err := s.Env().Config().GetStringMap("blob")
+	if !s.NoError(err) {
+		return
+	}
+
+	for storeName := range stores {
+		store := s.stores[storeName]
+
+		ch := make(chan struct{})
+		t := time.NewTicker(10 * time.Second)
+		var writeSuccess bool
+
+		go func() {
+			defer close(ch)
+
+			obj := &blob.Object{
+				Key:  mdl.Box("data"),
+				Body: blob.StreamBytes(bytes.NewBufferString(storeName).Bytes()),
+			}
+
+			err = store.WriteOne(obj)
+			writeSuccess = s.NoError(err)
+		}()
+
+		select {
+		case <-ch:
+		case <-t.C:
+			s.Fail("timeout")
+		}
+
+		if !writeSuccess {
+			s.Fail("failed to write object")
+		}
+
+		batch, err := store.ListObjects(s.T().Context(), "")
+		if !s.NoError(err) {
+			return
+		}
+
+		s.Len(batch, 1)
+	}
 }
 
 func TestStoreTestSuite(t *testing.T) {
