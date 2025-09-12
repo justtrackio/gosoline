f415d39fa789201151ecde08967d8549c299b691
Anselm Jonas Scholl <aj@justtrack.io>
2025-09-12 16:47:54 +0200

    stream: added support for snappy, lz4, and zstd for compressed messages;

--- pkg/stream/compression.go
+++ pkg/stream/compression.go
@@ -4,15 +4,17 @@ import (
 	"bytes"
 	"compress/gzip"
 	"fmt"
+
+	"github.com/golang/snappy"
+	"github.com/klauspost/compress/zstd"
+	"github.com/pierrec/lz4/v4"
 )
 
 type CompressionType string
 
 const (
-	CompressionNone CompressionType = "none"
-	CompressionGZip CompressionType = "application/gzip"
-
-	// compressors that are only provided externally (e.g. by kafka)
+	CompressionNone   CompressionType = "none"
+	CompressionGZip   CompressionType = "application/gzip"
 	CompressionSnappy CompressionType = "application/snappy"
 	CompressionLZ4    CompressionType = "application/lz4"
 	CompressionZstd   CompressionType = "application/zstd"
@@ -30,8 +32,11 @@ type MessageBodyCompressor interface {
 }
 
 var messageBodyCompressors = map[CompressionType]MessageBodyCompressor{
-	CompressionNone: new(noopCompressor),
-	CompressionGZip: new(gZipCompressor),
+	CompressionNone:   new(noopCompressor),
+	CompressionGZip:   new(gZipCompressor),
+	CompressionSnappy: new(snappyCompressor),
+	CompressionLZ4:    new(lz4Compressor),
+	CompressionZstd:   new(zstdCompressor),
 }
 
 func CompressMessage(compression CompressionType, body []byte) ([]byte, error) {
@@ -112,6 +117,138 @@ func (g gZipCompressor) Decompress(body []byte) ([]byte, error) {
 
 	if _, err := bufOut.ReadFrom(reader); err != nil {
 		return nil, fmt.Errorf("can not read from gzip reader: %w", err)
+	}
+
+	uncompressed := bufOut.Bytes()
+
+	return uncompressed, nil
+}
+
+type snappyCompressor struct{}
+
+func (g snappyCompressor) Compress(body []byte) ([]byte, error) {
+	if body == nil {
+		return nil, nil
+	}
+
+	var out bytes.Buffer
+	zw := snappy.NewBufferedWriter(&out)
+
+	if _, err := zw.Write(body); err != nil {
+		return nil, fmt.Errorf("can not write body to snappy: %w", err)
+	}
+
+	if err := zw.Close(); err != nil {
+		return nil, fmt.Errorf("can not close snappy writer: %w", err)
+	}
+
+	compressed := out.Bytes()
+
+	return compressed, nil
+}
+
+func (g snappyCompressor) Decompress(body []byte) ([]byte, error) {
+	if body == nil {
+		return nil, nil
+	}
+
+	bufBody := bytes.NewBuffer(body)
+	bufOut := &bytes.Buffer{}
+
+	reader := snappy.NewReader(bufBody)
+
+	if _, err := bufOut.ReadFrom(reader); err != nil {
+		return nil, fmt.Errorf("can not read from snappy reader: %w", err)
+	}
+
+	uncompressed := bufOut.Bytes()
+
+	return uncompressed, nil
+}
+
+type lz4Compressor struct{}
+
+func (g lz4Compressor) Compress(body []byte) ([]byte, error) {
+	if body == nil {
+		return nil, nil
+	}
+
+	var out bytes.Buffer
+	zw := lz4.NewWriter(&out)
+
+	if _, err := zw.Write(body); err != nil {
+		return nil, fmt.Errorf("can not write body to lz4: %w", err)
+	}
+
+	if err := zw.Close(); err != nil {
+		return nil, fmt.Errorf("can not close lz4 writer: %w", err)
+	}
+
+	compressed := out.Bytes()
+
+	return compressed, nil
+}
+
+func (g lz4Compressor) Decompress(body []byte) ([]byte, error) {
+	if body == nil {
+		return nil, nil
+	}
+
+	bufBody := bytes.NewBuffer(body)
+	bufOut := &bytes.Buffer{}
+
+	reader := lz4.NewReader(bufBody)
+
+	if _, err := bufOut.ReadFrom(reader); err != nil {
+		return nil, fmt.Errorf("can not read from lz4 reader: %w", err)
+	}
+
+	uncompressed := bufOut.Bytes()
+
+	return uncompressed, nil
+}
+
+type zstdCompressor struct{}
+
+func (g zstdCompressor) Compress(body []byte) ([]byte, error) {
+	if body == nil {
+		return nil, nil
+	}
+
+	var out bytes.Buffer
+	zw, err := zstd.NewWriter(&out)
+	if err != nil {
+		return nil, fmt.Errorf("can not create zstd writer: %w", err)
+	}
+
+	if _, err := zw.Write(body); err != nil {
+		return nil, fmt.Errorf("can not write body to zstd: %w", err)
+	}
+
+	if err := zw.Close(); err != nil {
+		return nil, fmt.Errorf("can not close zstd writer: %w", err)
+	}
+
+	compressed := out.Bytes()
+
+	return compressed, nil
+}
+
+func (g zstdCompressor) Decompress(body []byte) ([]byte, error) {
+	if body == nil {
+		return nil, nil
+	}
+
+	bufBody := bytes.NewBuffer(body)
+	bufOut := &bytes.Buffer{}
+
+	reader, err := zstd.NewReader(bufBody)
+	if err != nil {
+		return nil, fmt.Errorf("can not create zstd reader: %w", err)
+	}
+
+	if _, err := bufOut.ReadFrom(reader); err != nil {
+		return nil, fmt.Errorf("can not read from zstd reader: %w", err)
 	}
 
 	uncompressed := bufOut.Bytes()
