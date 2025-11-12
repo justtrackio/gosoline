0e6f54bf33de3a84b90314917b84b118860a0212
Fateme Afzoun <fateme.afzoun@justtrack.io>
2025-11-12 16:50:52 +0100

    delete `change_history` feature from `db-repo` with related models, migrations, and tests

--- test/db-repo/change_history/entity/migrations/1_initial.up.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
-create table items (
-    id int auto_increment primary key,
-    change_history_author_id int,
-    action varchar(10),
-    name varchar(10),
-    created_at datetime,
-    updated_at datetime
-);
