134a0a01a1c67ba5eb7c8bb21bd3a8ed8e2b1348
Anselm Jonas Scholl <aj@justtrack.io>
2026-04-13 09:35:30 +0200

    conc/ddb: handle cancel errors when releasing a lock;
    We extend a context until a lock expires, so if we for some reason (ddb
    slow) fail to release the lock in time, there is no point in propagating
    the cancel error as the lock has been released at that point anyway.

--- pkg/conc/ddb/ddb_lock_test.go
+++ pkg/conc/ddb/ddb_lock_test.go
@@ -30,7 +30,7 @@ func TestDdbLock(t *testing.T) {
 
 func (s *ddbLockTestSuite) SetupTest() {
 	s.lockManager = mocks.NewLockManager(s.T())
-	s.clock = clockPkg.NewFakeClock()
+	s.clock = clockPkg.NewFakeClockAt(time.Now())
 	s.logger = logMocks.NewLoggerMock(logMocks.WithTestingT(s.T()))
 	s.ctx = s.T().Context()
 	s.lock = ddb.NewDdbLockFromInterfaces(s.lockManager, s.clock, s.logger, s.ctx, "resource", "token", s.clock.Now().Add(time.Minute))
@@ -64,17 +64,43 @@ func (s *ddbLockTestSuite) TestReleaseLockFail() {
 	s.EqualError(err, "fail")
 }
 
-func (s *ddbLockTestSuite) TestReleaseLockTimeout() {
+func (s *ddbLockTestSuite) TestReleaseLockDelayedParentCancel() {
+	ctx, cancel := context.WithCancel(s.ctx)
+
+	s.lock = ddb.NewDdbLockFromInterfaces(s.lockManager, s.clock, s.logger, ctx, "resource", "token", s.clock.Now().Add(time.Minute))
+	cancel()
+
+	s.lockManager.EXPECT().ReleaseLock(matcher.Context, "resource", "token").
+		Run(func(ctx context.Context, resource string, token string) {
+			s.NoError(ctx.Err())
+		}).
+		Return(nil).
+		Once()
+
+	err := s.lock.Release()
+	s.NoError(err)
+}
+
+func (s *ddbLockTestSuite) TestReleaseLockCancellationIsIgnored() {
+	s.lock = ddb.NewDdbLockFromInterfaces(s.lockManager, s.clock, s.logger, s.ctx, "resource", "token", s.clock.Now().Add(time.Minute))
+
 	s.lockManager.EXPECT().
 		ReleaseLock(matcher.Context, "resource", "token").
-		Run(func(ctx context.Context, resource string, token string) {
-			s.clock.BlockUntilTimers(1)
-			s.clock.Advance(time.Minute)
-			<-ctx.Done()
-		}).
+		Return(context.Canceled).
+		Once()
+
+	err := s.lock.Release()
+	s.NoError(err)
+}
+
+func (s *ddbLockTestSuite) TestReleaseLockDeadlineExceededIsIgnored() {
+	s.lock = ddb.NewDdbLockFromInterfaces(s.lockManager, s.clock, s.logger, s.ctx, "resource", "token", s.clock.Now().Add(time.Minute))
+
+	s.lockManager.EXPECT().
+		ReleaseLock(matcher.Context, "resource", "token").
 		Return(context.DeadlineExceeded).
 		Once()
 
 	err := s.lock.Release()
-	s.EqualError(err, context.DeadlineExceeded.Error())
+	s.NoError(err)
 }
